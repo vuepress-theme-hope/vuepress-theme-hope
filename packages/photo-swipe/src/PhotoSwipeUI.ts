79a21f6199b02555e70d9ad9f458f375ce7ba6b7
Mr.Hope <zhangbowang1998@gmail.com>
2020-10-08 21:18:42 +0800

    feat(photo-swipe): compatable with Vue3

--- packages/photo-swipe/src/PhotoSwipeUI.ts
+++ packages/photo-swipe/src/PhotoSwipeUI.ts
@@ -1,80 +1,85 @@
 /* global IMAGE_CONTAINER, IMAGE_SELECTOR, PHOTOSWIPE_OPTIONS */
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { defineComponent, onMounted } from "@vue/composition-api";
 import PhotoSwipe from "photoswipe";
 import PhotoSwipeUIDefault from "photoswipe/dist/photoswipe-ui-default";
 
-let images: NodeListOf<HTMLImageElement>;
+export default defineComponent({
+  name: "PhotoSwipeUI",
 
-@Component
-export default class PhotoSwipeUI extends Vue {
-  private photoswipe(): void {
-    const pswp = document.querySelector(".pswp") as HTMLElement;
+  setup() {
+    let images: NodeListOf<HTMLImageElement>;
 
-    void this.getImages().then((imageConfig) => {
-      images.forEach((image, index) => {
-        image.onclick = (): void => {
-          const gallery = new PhotoSwipe(
-            pswp,
-            PhotoSwipeUIDefault,
-            imageConfig,
-            {
-              ...PHOTOSWIPE_OPTIONS,
-              index,
-            }
-          );
-          gallery.init();
-        };
-      });
-    });
-  }
-
-  private getImageInfo(
-    image: HTMLImageElement
-  ): PhotoSwipe.Item & { title: string } {
-    return {
+    const getImageInfo = (
+      image: HTMLImageElement
+    ): PhotoSwipe.Item & { title: string } => ({
       src: image.src,
       // eslint-disable-next-line id-length
       w: image.naturalWidth,
       h: image.naturalHeight,
       title: image.alt,
-    };
-  }
-
-  private getImages(): Promise<PhotoSwipe.Item[]> {
-    const promises: Promise<PhotoSwipe.Item & { title: string }>[] = [];
-    images = document.querySelectorAll<HTMLImageElement>(IMAGE_SELECTOR);
-
-    images.forEach((image, index) => {
-      promises[index] = new Promise((resolve, reject) => {
-        if (image.complete) resolve(this.getImageInfo(image));
-        else {
-          image.onload = (): void => resolve(this.getImageInfo(image));
-          image.onerror = (err): void => reject(err);
-        }
-      });
     });
 
-    return Promise.all(promises);
-  }
+    const getImages = (): Promise<PhotoSwipe.Item[]> => {
+      const promises: Promise<PhotoSwipe.Item & { title: string }>[] = [];
 
-  private mounted(): void {
-    const timer = setInterval(() => {
-      const content = document.querySelector<HTMLElement>(IMAGE_CONTAINER);
-      if (content) {
-        this.photoswipe();
-        clearInterval(timer);
-      }
-    }, 200);
-  }
+      images = document.querySelectorAll<HTMLImageElement>(IMAGE_SELECTOR);
 
-  @Watch("$route")
-  onRouteChange(): void {
-    const timer = setInterval(() => {
-      const content = document.querySelector<HTMLElement>(IMAGE_CONTAINER);
-      if (content) {
-        this.photoswipe();
-        clearInterval(timer);
-      }
-    }, 200);
-  }
-}
+      images.forEach((image, index) => {
+        promises[index] = new Promise((resolve, reject) => {
+          if (image.complete) resolve(getImageInfo(image));
+          else {
+            image.onload = (): void => resolve(getImageInfo(image));
+            image.onerror = (err): void => reject(err);
+          }
+        });
+      });
+
+      return Promise.all(promises);
+    };
+
+    const photoswipe = (): void => {
+      const pswp = document.querySelector(".pswp") as HTMLElement;
+
+      void getImages().then((imageConfig) => {
+        images.forEach((image, index) => {
+          image.onclick = (): void => {
+            const gallery = new PhotoSwipe(
+              pswp,
+              PhotoSwipeUIDefault,
+              imageConfig,
+              {
+                ...PHOTOSWIPE_OPTIONS,
+                index,
+              }
+            );
+            gallery.init();
+          };
+        });
+      });
+    };
+
+    onMounted(() => {
+      const timer = setInterval(() => {
+        const content = document.querySelector<HTMLElement>(IMAGE_CONTAINER);
+        if (content) {
+          photoswipe();
+          clearInterval(timer);
+        }
+      }, 200);
+    });
+
+    return { photoswipe };
+  },
+
+  watch: {
+    $route(): void {
+      const timer = setInterval(() => {
+        const content = document.querySelector<HTMLElement>(IMAGE_CONTAINER);
+        if (content) {
+          this.photoswipe();
+          clearInterval(timer);
+        }
+      }, 200);
+    },
+  },
+});
--- packages/photo-swipe/src/enhanceAppFile.ts
+++ packages/photo-swipe/src/enhanceAppFile.ts
@@ -1,8 +1,10 @@
-import { EnhanceApp } from "@mr-hope/vuepress-types";
+import VueCompositionAPI from "@vue/composition-api";
 import PhotoSwipeUI from "./PhotoSwipeUI.vue";
+import { EnhanceApp } from "@mr-hope/vuepress-types";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const enhanceApp: EnhanceApp = ({ Vue }) => {
+  Vue.use(VueCompositionAPI);
   Vue.component("PhotoSwipeUI", PhotoSwipeUI);
 };
 
