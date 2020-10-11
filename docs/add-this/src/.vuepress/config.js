40b46ef7b06035bab00fa522564e081a2dbe0aca
Mr.Hope <zhangbowang1998@gmail.com>
2020-10-11 16:07:02 +0800

    docs(add-this): fix locate issues

--- docs/add-this/src/.vuepress/config.js
+++ docs/add-this/src/.vuepress/config.js
@@ -32,9 +32,26 @@ module.exports = resolve({
     /** 网站的logo */
     logo: "/logo.svg",
 
+    author: "Mr.Hope",
+    iconPrefix: "vuepress-",
+
     footer: {
       display: true,
       copyright: "MIT Licensed | Copyright © 2019-present Mr.Hope",
+    },
+
+    markdown: {
+      enableAll: true,
+    },
+
+    comment: {
+      type: "valine",
+      appId: "EBfvFXOPGWKfEvTXSsw1RnXy-gzGzoHsz",
+      appKey: "M8y71yAlPuW2RQJ83r15qfk9",
+    },
+
+    locales: {
+      "/en/": {},
     },
 
     addthis: "ra-5f829c59e6c6bc9a",
