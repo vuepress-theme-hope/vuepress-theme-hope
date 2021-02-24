6965b3ebd081693626e16ba215a8c90926f51400
Mr.Hope <zhangbowang1998@gmail.com>
2021-02-25 01:05:53 +0800

    feat(component): update CodeGroup appearance

--- packages/components/src/client/CodeGroup.vue
+++ packages/components/src/client/CodeGroup.vue
@@ -33,15 +33,12 @@
 
 <style lang="stylus">
 .code-group-nav
-  margin-top 0.85rem
   /* 2 * margin + border-radius of <pre> tag */
-  margin-bottom calc(-1.7rem - 6px)
-  padding-bottom calc(1.7rem - 6px)
-  padding-left 10px
-  padding-top 10px
+  margin 0.85rem 0 calc(-1.7rem - 7px)
+  padding 0 5px calc(1.7rem - 7px)
   border-top-left-radius 6px
   border-top-right-radius 6px
-  background-color var(--code-bgcolor, #282c34)
+  background-color background-color darken(saturate(#282c34, 10%), 35%)
 
   @media (max-width $MQMobileNarrow)
     margin-left -1.5rem
@@ -55,18 +52,44 @@
   list-style none
 
 .code-group-nav-tab
+  position relative
   border 0
-  padding 5px
+  padding 6px 10px
   cursor pointer
+  border-top-left-radius 8px
+  border-top-right-radius 8px
   background-color transparent
   font-size 0.85em
   line-height 1.4
   color var(--text-color, $textColor)
   font-weight 600
 
+  &:hover
+    background-color darken(saturate(#282c34, 10%), 5%)
+
+  &::before, &::after
+    position absolute
+    z-index 1
+    content ' '
+    bottom 0
+    width 8px
+    height 8px
+
+  &::before
+    right 100%
+
+  &::after
+    left 100%
+
   &:focus
     outline none
 
   &.active
     border-bottom var(--accent-color, $accentColor) 1px solid
+
+    &:before
+      background radial-gradient(16px at left top, transparent 50%, #282c34 50%)
+
+    &:after
+      background radial-gradient(16px at right top, transparent 50%, #282c34 50%)
 </style>
--- packages/components/src/client/CodeGroupItem.vue
+++ packages/components/src/client/CodeGroupItem.vue
@@ -12,4 +12,8 @@
 
   &.active
     display block
+
+  div[class*='language-']
+    border-top-left-radius 0
+    border-top-right-radius 0
 </style>
