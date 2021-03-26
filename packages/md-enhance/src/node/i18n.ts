a41d74d7b09838091a8ec2ff0adc8c67f455d511
Michael Bischof <michael@nextpart.io>
2021-03-26 22:55:02 +0100

    added missing german translation in components

--- packages/md-enhance/src/node/i18n.ts
+++ packages/md-enhance/src/node/i18n.ts
@@ -2,26 +2,31 @@ export const i18n = {
   info: {
     "/zh/": "相关信息",
     "/en/": "Info",
+    "/de/": "Information",
     "/vi/": "Thông tin",
   },
   tip: {
     "/zh/": "提示",
     "/en/": "Tips",
+    "/de/": "Tips",
     "/vi/": "Tips",
   },
   warning: {
     "/zh/": "注意",
     "/en/": "Note",
+    "/de/": "Notiz",
     "/vi/": "Lưu ý",
   },
   danger: {
     "/zh/": "警告",
     "/en/": "Warning",
+    "/de/": "Warnung",
     "/vi/": "Cẩn thận",
   },
   detail: {
     "/zh/": "详情",
     "/en/": "Detail",
+    "/de/": "Details",
     "/vi/": "Chi tiết",
   },
 };
