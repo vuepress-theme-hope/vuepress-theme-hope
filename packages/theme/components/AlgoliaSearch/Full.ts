aab88d709349cd88837e901508991b1dcce374fa
Mr.Hope <zhangbowang1998@gmail.com>
2021-05-08 14:27:46 +0800

    feat(theme): update docsearch to V3

--- packages/theme/components/AlgoliaSearch/Full.ts
+++ packages/theme/components/AlgoliaSearch/Full.ts
@@ -1,4 +1,8 @@
+import { createElement } from "preact";
 import Vue from "vue";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: docsearch type issue
+import docsearch from "@docsearch/js";
 
 import type { AlgoliaOption } from "@mr-hope/vuepress-types";
 import type { DocSearchProps } from "@docsearch/react";
@@ -28,87 +32,83 @@ export default Vue.extend({
   methods: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     initialize(userOptions: AlgoliaOption, _lang: string): void {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      void import(/* webpackChunkName: "docsearch" */ "@docsearch/js").then(
-        (docsearch) => {
-          // eslint-disable-next-line
-          (docsearch as any).default({
-            container: "#docsearch",
-            placeholder:
-              (this.$site.themeConfig.searchPlaceholder as string) || "",
-            ...userOptions,
-            searchParameters: userOptions.searchParameters || {},
+      // eslint-disable-next-line
+      (docsearch as (props: DocSearchProps & { container: string }) => void)({
+        container: "#docsearch",
+        placeholder: (this.$site.themeConfig.searchPlaceholder as string) || "",
+        ...userOptions,
+        searchParameters: userOptions.searchParameters || {},
 
-            transformItems: (items) =>
-              items.map((item) => ({
-                ...item,
-                url: this.getRelativePath(item.url),
-              })),
+        // transform full url to route path
+        transformItems: (items) =>
+          items.map((item) => ({
+            ...item,
+            // the `item.url` is full url with protocol and hostname
+            // so we have to transform it to vue-router path
+            url: this.resolveRoutePathFromUrl(item.url),
+          })),
 
-            hitComponent: ({ hit, children }) => ({
-              type: "a",
-              key: null,
-              ref: undefined,
-              constructor: undefined,
-              props: {
-                href: hit.url,
-                onClick: (event: Event): void => {
-                  // We rely on the native link scrolling when user is
-                  // already on the right anchor because Vue Router doesn’t
-                  // support duplicated history entries.
-                  if (this.$route.fullPath === hit.url) return;
+        // render the hit component with custom `onClick` handler
+        hitComponent: ({ hit, children }) =>
+          createElement(
+            "a",
+            {
+              href: hit.url,
+              onClick: (event: Event): void => {
+                // We rely on the native link scrolling when user is
+                // already on the right anchor because Vue Router doesn’t
+                // support duplicated history entries.
+                if (this.$route.fullPath === hit.url) return;
 
-                  const fullPath = `${window.location.origin}${hit.url}`;
-                  const { pathname: hitPathname } = new URL(fullPath);
-                  // If the hits goes to another page, we prevent the native link behavior
-                  // to leverage the Vue Router loading feature.
-                  if (this.$route.path !== hitPathname) event.preventDefault();
-
-                  if (
-                    this.$router
-                      .getRoutes()
-                      .some((route) => route.path === hitPathname)
-                  )
-                    void this.$router.push(hit.url);
-                  else window.open(fullPath);
-                },
-                children,
-              },
-            }),
-
-            navigator: {
-              navigate: ({ suggestionUrl }): void => {
-                const fullPath = `${window.location.origin}${suggestionUrl}`;
+                const fullPath = `${window.location.origin}${hit.url}`;
                 const { pathname: hitPathname } = new URL(fullPath);
-                // Vue Router doesn’t handle same-page navigation so we use
-                // the native browser location API for anchor navigation.
-                if (this.$route.path === hitPathname)
-                  window.location.assign(fullPath);
-                else if (
+                // If the hits goes to another page, we prevent the native link behavior
+                // to leverage the Vue Router loading feature.
+                if (this.$route.path !== hitPathname) event.preventDefault();
+
+                if (
                   this.$router
                     .getRoutes()
                     .some((route) => route.path === hitPathname)
                 )
-                  void this.$router.push(suggestionUrl);
+                  void this.$router.push(hit.url);
                 else window.open(fullPath);
               },
-              navigateNewTab({ suggestionUrl }): void {
-                window.open(suggestionUrl);
-              },
-              navigateNewWindow({ suggestionUrl }): void {
-                window.open(suggestionUrl);
-              },
             },
-          } as DocSearchProps);
-        }
-      );
+            children
+          ),
+
+        navigator: {
+          navigate: ({ itemUrl }): void => {
+            const fullPath = `${window.location.origin}${itemUrl}`;
+            const { pathname: hitPathname } = new URL(fullPath);
+
+            // Vue Router doesn’t handle same-page navigation so we use
+            // the native browser location API for anchor navigation.
+            if (this.$route.path === hitPathname)
+              window.location.assign(fullPath);
+            else if (
+              this.$router
+                .getRoutes()
+                .some((route) => route.path === hitPathname)
+            )
+              void this.$router.push(itemUrl);
+            else window.open(fullPath);
+          },
+          navigateNewTab({ itemUrl }): void {
+            window.open(itemUrl);
+          },
+          navigateNewWindow({ itemUrl }): void {
+            window.open(itemUrl);
+          },
+        },
+      });
     },
 
-    getRelativePath(absoluteUrl: string): string {
+    resolveRoutePathFromUrl(absoluteUrl: string): string {
       const { pathname, hash } = new URL(absoluteUrl);
-      const url = pathname.replace(this.$site.base, "/") + hash;
-      return url;
+
+      return `${pathname.replace(this.$site.base, "/")}${hash}`;
     },
 
     update(options: AlgoliaOption, lang: string): void {
