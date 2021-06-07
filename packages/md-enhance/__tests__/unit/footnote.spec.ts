2b2e40eea9a920b88685edf2f9b7a0278e428c58
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-07 11:35:38 +0800

    docs: fix typos

--- packages/md-enhance/__tests__/unit/footnote.spec.ts
+++ packages/md-enhance/__tests__/unit/footnote.spec.ts
@@ -69,7 +69,7 @@ baz
     expect(
       markdownIt.render(`
 Here is an inline note.^[Inlines notes are easier to write, since
-you don't have to pick an identifier and move down to type the
+you don’t have to pick an identifier and move down to type the
 note.]
 `)
     ).toMatchSnapshot();
