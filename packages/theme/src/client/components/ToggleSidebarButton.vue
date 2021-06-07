4f65d259e0ac1ada1a914740a1e7b5f66746ed01
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-07 16:54:20 +0800

    feat(theme): update toggle sidebar button

--- packages/theme/src/client/components/ToggleSidebarButton.vue
+++ packages/theme/src/client/components/ToggleSidebarButton.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="toggle-sidebar-button" @click="$emit('toggle')">
-    <svg
-      class="icon"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      role="img"
-      viewBox="0 0 448 512"
-    >
-      <path
-        fill="currentColor"
-        d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"
-      />
-    </svg>
-  </div>
+  <button
+    class="toggle-sidebar-button"
+    title="Toggle Sidebar"
+    @click="$emit('toggle')"
+  >
+    <span class="icon" />
+  </button>
 </template>
 
 <script lang="ts">
