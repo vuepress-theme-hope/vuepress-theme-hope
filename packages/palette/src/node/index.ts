754819f01a1c89c49d588463648b65569ef754dd
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-11 17:35:00 +0800

    feat(palette): rebuild plugin

--- packages/palette/src/node/index.ts
+++ packages/palette/src/node/index.ts
@@ -3,7 +3,9 @@ import type { Plugin } from "@vuepress/core";
 
 export interface PaletteConfig {
   config?: string;
+  defaultConfig?: string;
   palette?: string;
+  defaultPalette?: string;
   style?: string;
 }
 
@@ -12,7 +14,9 @@ const emptyFile = path.resolve(__dirname, "../styles/empty.scss");
 export const palettePlugin: Plugin<PaletteConfig> = (
   {
     config = ".vuepress/styles/hope-config.scss",
+    defaultConfig = path.resolve(__dirname, "../styles/default/config.scss"),
     palette = ".vuepress/styles/hope-palette.scss",
+    defaultPalette = path.resolve(__dirname, "../styles/default/palette.scss"),
     style = ".vuepress/styles/hope-style.scss",
   },
   app
@@ -20,18 +24,61 @@ export const palettePlugin: Plugin<PaletteConfig> = (
   const userConfig = app.dir.source(config);
   const userPalette = app.dir.source(palette);
   const userStyle = app.dir.source(style);
+  const getPath = (path: string): string =>
+    fs.pathExistsSync(path) ? path : emptyFile;
 
   return {
     name: "@mr-hope/palette",
 
     alias: {
-      "@user/config": fs.pathExistsSync(userConfig) ? userConfig : emptyFile,
-      "@user/palette": fs.pathExistsSync(userPalette) ? userPalette : emptyFile,
-      "@user/style": fs.pathExistsSync(userStyle) ? userStyle : emptyFile,
-      "@hope/config": path.resolve(__dirname, "../styles/config.scss"),
-      "@hope/palette": path.resolve(__dirname, "../styles/palette.scss"),
+      "@hope/config": "@temp/hope/config.scss",
+      "@hope/palette": "@temp/hope/palette.scss",
       "@hope/helper": path.resolve(__dirname, "../styles/helper.scss"),
-      "@hope/style": path.resolve(__dirname, "../styles/style.scss"),
+      "@hope/style": getPath(userStyle),
+    },
+
+    async onPrepared(): Promise<void> {
+      await app.writeTemp(
+        "hope/config.scss",
+        `
+@import "${getPath(defaultPalette)}";
+@import "${getPath(defaultConfig)}";
+@import "${getPath(userPalette)}";
+@import "${getPath(userConfig)}";
+`
+      );
+      await app.writeTemp(
+        "hope/palette.scss",
+        `
+@use 'sass:color';
+@use 'sass:list';
+@use 'sass:math';
+@use 'sass:map';
+@use 'sass:meta';
+
+@use '@hope/helper';
+@use '${getPath(defaultPalette)}' as defaultPalette;
+@use '${getPath(palette)}' as palette;
+
+$defaultVariables: meta.module-variables("defaultPalette");
+$userVariables: meta.module-variables("palette");
+
+$variables: map.deep-merge($defaultVariables, $userVariables);
+
+@each $name, $value in $variables {
+  $key: helper.camel-to-kebab($name);
+
+  // simple length
+  @if meta.type-of($value) == number or meta.type-of($value) == string {
+    :root {
+      #{$key}: #{$value};
+    }
+  } @else if helper.color-islegal($value) {
+    @include helper.inject-color($key, $value, $darkSelector: "html.dark");
+  }
+}
+`
+      );
     },
   };
 };
