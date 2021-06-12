681df721fcea544bcb716ab3591a22110d293479
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-12 12:08:45 +0800

    chore: update packages folder

--- packages/sass-palette/src/node/options.ts
+++ packages/sass-palette/src/node/options.ts
@@ -1,12 +1,14 @@
 export interface PaletteOptions {
   /** identifier for palette */
   id: string;
+
   /**
    * user config file path, relative to source dir
    *
    * @default ".vuepress/styles/config.scss"
    */
   config?: string;
+
   /**
    * default config file path, should be absolute path
    *
@@ -15,12 +17,14 @@ export interface PaletteOptions {
    * @default "vuepress-plugin-sass-palette/styles/default/config.scss"
    */
   defaultConfig?: string;
+
   /**
    * user palette file path, relative to source dir
    *
    * @default ".vuepress/styles/palette.scss"
    */
   palette?: string;
+
   /**
    * default palette file path, should be absolute path
    *
@@ -30,6 +34,7 @@ export interface PaletteOptions {
    */
 
   defaultPalette?: string;
+
   /**
    * user style file path, relative to source dir
    *
