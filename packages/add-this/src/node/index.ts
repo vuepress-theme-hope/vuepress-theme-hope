af918a88afbb53a239afb1a5ba1a95c70a7bdddb
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-12 10:01:33 +0800

    chore(add-this): code refactor

--- packages/add-this/src/node/index.ts
+++ packages/add-this/src/node/index.ts
@@ -3,7 +3,7 @@ import { path } from "@vuepress/utils";
 import type { Plugin } from "@vuepress/core";
 import type { AddThisOptions } from "../shared";
 
-export * from "../shared/types";
+export * from "../shared";
 
 const addThisPlugin: Plugin<AddThisOptions> = (options) => ({
   name: "add-this",
