1f16620fb60604b105c9313a2a9332d42b5a5b7b
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-13 21:36:13 +0800

    style: fix eslint warnings

--- packages/md-enhance/src/shims-reveal.d.ts
+++ packages/md-enhance/src/shims-reveal.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "reveal.js" {
   export interface RevealOptions {
