4e7735294e9394be3d736add0e3b3aa682cc968a
Mr.Hope <zhangbowang1998@gmail.com>
2021-08-07 21:33:27 +0800

    refactor(theme): update theme export

--- packages/theme/src/node/index.ts
+++ packages/theme/src/node/index.ts
@@ -2,7 +2,6 @@ import { themeHope } from "./theme";
 
 export * from "./defaultLocaleAssign";
 export * from "./theme";
-export * from "./plugins";
 export * from "../shared";
 
 export default themeHope;
