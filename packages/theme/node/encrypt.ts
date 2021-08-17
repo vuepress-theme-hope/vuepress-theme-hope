3b720ad789e209198c61f6abf01b2bb995b65e07
Mr.Hope <zhangbowang1998@gmail.com>
2021-08-17 22:19:22 +0800

    feat(theme): update encrypt hint

--- packages/theme/node/encrypt.ts
+++ packages/theme/node/encrypt.ts
@@ -8,14 +8,21 @@ export const resolveEncrypt = (encrypt: EncryptOptions): void => {
     if (typeof encrypt.global === "string")
       encrypt.global = hashSync(encrypt.global, 10);
     else if (Array.isArray(encrypt.global))
-      encrypt.global = encrypt.global.map((globalPassword) =>
-        hashSync(globalPassword, 10)
-      );
+      encrypt.global = encrypt.global.map((globalPassword) => {
+        if (typeof globalPassword === "string")
+          return hashSync(globalPassword, 10);
+
+        throw new Error(
+          `[vuepress-theme-hope]: You config "themeConfig.encrypt.global", but your config is invalid. 
+
+          All password MUST be string. But we found one’s type is ${typeof globalPassword}. Please fix it!`
+        );
+      });
     else
       throw new Error(
-        `[vuepress-theme-hope]: You asked for global Encrtption but you did not specific valid global Password. 
+        `[vuepress-theme-hope]: You are asking for global encryption but you provide invalid "global" config. 
 
-Please add "global" option your "themeConfig.encrypt" config. It can be string or string[].`
+Please check "global" in your "themeConfig.encrypt" config. It can be string or string[], but you are providing ${typeof encrypt.global}. Please fix it!`
       );
 
   const passwordConfig = encrypt.config || {};
@@ -32,13 +39,13 @@ Please add "global" option your "themeConfig.encrypt" config. It can be string o
 
         throw new Error(`[vuepress-theme-hope]: You config "themeConfig.encrypt.config", but your config is invalid. 
         
-Key ${key}’s value MUST be string or string[]. But it’s type is ${typeof configPassword}. Please fix it`);
+Key ${key}’s value MUST be string or string[]. But it’s type is ${typeof configPassword}. Please fix it!`);
       });
     else
       throw new Error(
         `[vuepress-theme-hope]: You config "themeConfig.encrypt.config", but your config is invalid. 
 
-The value of key ${key} MUST be string or string[]. But not it’s ${typeof password}. Please fix it`
+The value of key ${key} MUST be string or string[]. But not it’s ${typeof password}. Please fix it!`
       );
   });
 };
