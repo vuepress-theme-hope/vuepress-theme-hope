d8c0ba9030232eadf491378a8741b4d24e379d70
Mr.Hope <zhangbowang1998@gmail.com>
2021-10-31 17:21:41 +0800

    fix(theme): fix 404 layout

--- packages/theme/src/client/components/icons/Page404.vue
+++ packages/theme/src/client/components/icons/Page404.vue
@@ -180,3 +180,11 @@
     </g>
   </svg>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Page404",
+});
+</script>
