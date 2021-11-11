9954c65fa85db6e887447c2466ba1f81d732383d
Mr.Hope <zhangbowang1998@gmail.com>
2021-11-12 00:54:29 +0800

    feat(create): update registry options

--- packages/create/src/registry.ts
+++ packages/create/src/registry.ts
@@ -4,10 +4,10 @@ import { prompt } from "inquirer";
 import type { Lang } from "./i18n";
 
 export interface RegistryAnswer {
-  registry: "淘宝源" | "当前源";
+  registry: "国内镜像源" | "当前源";
 }
 
-const taobaoRegistry = "https://registry.npm.taobao.org/";
+const npmmirrorRegistry = "https://registry.npmmirror.com/";
 
 const getUserRegistry = (bin: string): string =>
   sync(bin ? "yarn" : "npm", ["config", "get", "registry"]).stdout;
@@ -15,16 +15,17 @@ const getUserRegistry = (bin: string): string =>
 export const getRegistry = async (lang: Lang, bin: string): Promise<string> => {
   const userRegistry = getUserRegistry(bin);
 
-  if (lang === "en-US" || userRegistry === taobaoRegistry) return userRegistry;
+  if (lang === "en-US" || userRegistry === npmmirrorRegistry)
+    return userRegistry;
 
   const { registry } = await prompt<RegistryAnswer>([
     {
       name: "registry",
       type: "list",
       message: "选择你想使用的源",
-      choices: ["淘宝源", "当前源"],
+      choices: ["国内镜像源", "当前源"],
     },
   ]);
 
-  return registry === "淘宝源" ? taobaoRegistry : userRegistry;
+  return registry === "国内镜像源" ? npmmirrorRegistry : userRegistry;
 };
