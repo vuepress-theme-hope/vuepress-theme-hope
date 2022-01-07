8b4118f11d8fd45ca5dd410744517c1f34d056ff
Mr.Hope <zhangbowang1998@gmail.com>
2022-01-08 00:26:50 +0800

    chore: update build script

--- demo/src/.vuepress/config.ts
+++ demo/src/.vuepress/config.ts
@@ -45,12 +45,6 @@ export default defineUserConfig<HopeThemeOptions>({
     },
   },
 
-  bundler:
-    // specify bundler via environment variable
-    process.env.DOCS_BUNDLER ??
-    // use vite in dev, use webpack in prod
-    (isProd ? "@vuepress/webpack" : "@vuepress/vite"),
-
   theme: "hope",
 
   themeConfig: {
