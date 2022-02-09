03a894303de84a26f4c6a96d6c1976fa39159b2f
Mr.Hope <zhangbowang1998@gmail.com>
2022-02-09 15:42:41 +0800

    fix(theme): fix sidebar nav dropdown

--- packages/theme/src/client/module/sidebar/components/SidebarDropdownLink.ts
+++ packages/theme/src/client/module/sidebar/components/SidebarDropdownLink.ts
@@ -45,92 +45,88 @@ export default defineComponent({
     const isLastItemOfArray = <T>(item: T, arr: T[]): boolean =>
       arr[arr.length - 1] === item;
 
-    return (): VNode =>
-      h("div", { class: ["mobile-dropdown-wrapper", { open: open.value }] }, [
-        h(
-          "button",
-          {
-            class: "dropdown-title",
-            type: "button",
-            ariaLabel: dropdownAriaLabel.value,
-            onClick: () => {
-              open.value = !open.value;
-            },
+    return (): VNode[] => [
+      h(
+        "button",
+        {
+          class: "mobile-dropdown-title",
+          type: "button",
+          ariaLabel: dropdownAriaLabel.value,
+          onClick: () => {
+            open.value = !open.value;
           },
-          [
-            slots.title?.() ||
-              h("span", { class: "title" }, [
-                config.value.icon
-                  ? h("i", {
-                      class: `icon ${iconPrefix.value}${config.value.icon}`,
-                    })
-                  : null,
-                props.config.text,
-              ]),
-            h("span", { class: ["arrow", open.value ? "down" : "right"] }),
-          ]
-        ),
-        h(
-          "ul",
-          {
-            class: ["nav-dropdown", { hide: !open.value }],
-          },
-          config.value.children.map((child) =>
-            h(
-              "li",
-              { class: "dropdown-item" },
-              "children" in child
-                ? [
-                    h(
-                      "h4",
-                      { class: "dropdown-subtitle" },
-                      child.link
-                        ? h(AutoLink, {
-                            config: child as AutoLinkType,
-                            onFocusout: () => {
-                              if (
-                                isLastItemOfArray(
-                                  child,
-                                  config.value.children
-                                ) &&
-                                child.children.length === 0
-                              )
-                                open.value = false;
-                            },
-                          })
-                        : h("span", child.text)
-                    ),
-                    h(
-                      "ul",
-                      { class: "dropdown-subitem-wrapper" },
-                      child.children.map((grandchild) =>
-                        h(
-                          "li",
-                          { class: "dropdown-subitem" },
-                          h(AutoLink, {
-                            config: grandchild,
-                            onFocusout: () => {
-                              if (
-                                isLastItemOfArray(grandchild, child.children) &&
-                                isLastItemOfArray(child, config.value.children)
-                              )
-                                open.value = false;
-                            },
-                          })
-                        )
-                      )
-                    ),
-                  ]
-                : h(AutoLink, {
-                    config: child,
-                    onFocusout: () => {
-                      if (isLastItemOfArray(child, config.value.children))
-                        open.value = false;
-                    },
+        },
+        [
+          slots.title?.() ||
+            h("span", { class: "title" }, [
+              config.value.icon
+                ? h("i", {
+                    class: `icon ${iconPrefix.value}${config.value.icon}`,
                   })
-            )
+                : null,
+              props.config.text,
+            ]),
+          h("span", { class: ["arrow", open.value ? "down" : "right"] }),
+        ]
+      ),
+      h(
+        "ul",
+        {
+          class: ["mobile-nav-dropdown", { hide: !open.value }],
+        },
+        config.value.children.map((child) =>
+          h(
+            "li",
+            { class: "dropdown-item" },
+            "children" in child
+              ? [
+                  h(
+                    "h4",
+                    { class: "dropdown-subtitle" },
+                    child.link
+                      ? h(AutoLink, {
+                          config: child as AutoLinkType,
+                          onFocusout: () => {
+                            if (
+                              isLastItemOfArray(child, config.value.children) &&
+                              child.children.length === 0
+                            )
+                              open.value = false;
+                          },
+                        })
+                      : h("span", child.text)
+                  ),
+                  h(
+                    "ul",
+                    { class: "dropdown-subitem-wrapper" },
+                    child.children.map((grandchild) =>
+                      h(
+                        "li",
+                        { class: "dropdown-subitem" },
+                        h(AutoLink, {
+                          config: grandchild,
+                          onFocusout: () => {
+                            if (
+                              isLastItemOfArray(grandchild, child.children) &&
+                              isLastItemOfArray(child, config.value.children)
+                            )
+                              open.value = false;
+                          },
+                        })
+                      )
+                    )
+                  ),
+                ]
+              : h(AutoLink, {
+                  config: child,
+                  onFocusout: () => {
+                    if (isLastItemOfArray(child, config.value.children))
+                      open.value = false;
+                  },
+                })
           )
-        ),
-      ]);
+        )
+      ),
+    ];
   },
 });
