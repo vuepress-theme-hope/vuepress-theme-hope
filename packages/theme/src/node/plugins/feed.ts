0d902b5d6ad93d79cfe90ca4baaea5c5ce749054
Mr.Hope <zhangbowang1998@gmail.com>
2022-02-14 19:23:40 +0800

    feat(theme): update feed option

--- packages/theme/src/node/plugins/feed.ts
+++ packages/theme/src/node/plugins/feed.ts
@@ -7,8 +7,8 @@ export const resolveFeedOptions = (
 ): FeedOptions | false => {
   if (options === false) return false;
 
-  // disable feed if `hostname` is not set and no options for feed plugin
-  if (!Object.keys(options || {}).length && !themeConfig.hostname) return false;
+  // disable feed if no options for feed plugin
+  if (!Object.keys(options || {}).length) return false;
 
   return {
     hostname: themeConfig.hostname,
