3eb0c6ed49370ab21e0490cd1bfb45a2142380b8
Mr.Hope <zhangbowang1998@gmail.com>
2022-02-19 21:04:20 +0800

    chore(copy-code2): update locale jsdoc

--- packages/copy-code2/src/shared/locales.ts
+++ packages/copy-code2/src/shared/locales.ts
@@ -2,16 +2,16 @@ import type { ConvertLocaleConfig } from "@mr-hope/vuepress-shared";
 
 export interface CopyCodeLocaleData {
   /**
-   * 复制按钮文字
-   *
    * Copy button label text
+   *
+   * 复制按钮文字
    */
   copy: string;
 
   /**
-   * 复制成功提示消息文字
-   *
    * Success message text after content is copied
+   *
+   * 复制成功提示消息文字
    */
   hint: string;
 }
