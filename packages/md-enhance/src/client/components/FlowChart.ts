e7c5139692c0d665a1fa96a0308cf00500fcbe43
Mr.Hope <zhangbowang1998@gmail.com>
2022-03-12 16:50:10 +0800

    refactor(md-enhance): render id with render function

--- packages/md-enhance/src/client/components/FlowChart.ts
+++ packages/md-enhance/src/client/components/FlowChart.ts
@@ -7,6 +7,7 @@ import {
   ref,
 } from "vue";
 import debounce from "lodash.debounce";
+import { loadingSvgString } from "./icons";
 import presets from "../flowchart-preset";
 
 import type * as Flowchart from "flowchart.js";
@@ -52,8 +53,6 @@ export default defineComponent({
       width < 419 ? 0.8 : width > 1280 ? 1 : 0.9;
 
     onMounted(() => {
-      element.value?.setAttribute("id", props.id);
-
       void Promise.all([
         import(/* webpackChunkName: "flowchart" */ "flowchart.js"),
         // delay
@@ -93,13 +92,13 @@ export default defineComponent({
       loading.value
         ? h("div", {
             class: ["flowchart-loading-wrapper"],
-            innerHTML:
-              '<svg xmlns="http://www.w3.org/2000/svg" class="loading-icon" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><circle cx="50" cy="50" r="0" fill="none" stroke="currentColor" stroke-width="2"><animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s"/><animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s"/></circle><circle cx="50" cy="50" r="0" fill="none" stroke="currentColor" stroke-width="2"><animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.3333333333333333s"/><animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.3333333333333333s"/></circle><circle cx="50" cy="50" r="0" fill="none" stroke="currentColor" stroke-width="2"><animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.6666666666666666s"/><animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.6666666666666666s"/></circle></svg>',
+            innerHTML: loadingSvgString,
           })
         : null,
       h("div", {
         ref: element,
         class: ["flowchart-wrapper", props.preset],
+        id: props.id,
         style: {
           display: loading.value ? "none" : "block",
         },
