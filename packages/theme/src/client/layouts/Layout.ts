0b7c6cdcdea1f7b49b1d52575b7e9ffa5d1050b8
Mr.Hope <zhangbowang1998@gmail.com>
2022-03-19 01:07:18 +0800

    fix(theme): fix build issue when blog is not enabled

--- packages/theme/src/client/layouts/Layout.ts
+++ packages/theme/src/client/layouts/Layout.ts
@@ -1,10 +1,10 @@
-import { defineComponent, h, resolveComponent } from "vue";
+import { computed, defineComponent, h, resolveComponent } from "vue";
 import { usePageData, usePageFrontmatter } from "@vuepress/client";
 
 import { FadeSlideY } from "@theme-hope/components/transitions";
 import SkipLink from "@theme-hope/components/SkipLink";
 import { useMobile } from "@theme-hope/composables";
-import { useBlogOptions } from "@theme-hope/module/blog/composables";
+import { useThemeData, useThemeLocaleData } from "@theme-hope/composables";
 
 import type { ComponentOptions, VNode } from "vue";
 import type { HopeThemePageFrontmatter } from "../../shared";
@@ -16,10 +16,17 @@ export default defineComponent({
   name: "Layout",
 
   setup() {
+    const themeData = useThemeData();
+    const themeLocale = useThemeLocaleData();
     const page = usePageData();
     const frontmatter = usePageFrontmatter<HopeThemePageFrontmatter>();
-    const blogOptions = useBlogOptions();
     const isMobile = useMobile();
+
+    const sidebarDisplay = computed(
+      () =>
+        themeLocale.value.blog.sidebarDisplay ||
+        themeData.value.blog.sidebarDisplay
+    );
 
     return (): VNode[] => [
       h(SkipLink),
@@ -33,12 +40,12 @@ export default defineComponent({
               : h(FadeSlideY, () =>
                   h(resolveComponent("NormalPage"), { key: page.value.path })
                 ),
-          ...(ENABLE_BLOG && blogOptions.value.sidebarDisplay !== "none"
+          ...(ENABLE_BLOG && sidebarDisplay.value !== "none"
             ? { navScreenBottom: () => h(resolveComponent("BloggerInfo")) }
             : {}),
           ...(ENABLE_BLOG &&
           !isMobile.value &&
-          blogOptions.value.sidebarDisplay == "always"
+          sidebarDisplay.value === "always"
             ? { sidebar: () => h(resolveComponent("BloggerInfo")) }
             : {}),
         }
