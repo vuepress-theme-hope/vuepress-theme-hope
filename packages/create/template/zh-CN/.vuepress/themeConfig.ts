0daf3cbdd0775a0670819dbb05eafac192fa1a63
Mr.Hope <zhangbowang1998@gmail.com>
2022-03-31 00:59:07 +0800

    feat(create): update template comment config

--- packages/create/template/zh-CN/.vuepress/themeConfig.ts
+++ packages/create/template/zh-CN/.vuepress/themeConfig.ts
@@ -14,7 +14,7 @@ export default defineThemeConfig({
 
   logo: "/logo.svg",
 
-  repo: "https://github.com/vuepress-theme-hope/vuepress-theme-hope",
+  repo: "vuepress-theme-hope/vuepress-theme-hope",
 
   docsDir: "demo/src",
 
@@ -77,13 +77,30 @@ export default defineThemeConfig({
       autoExcerpt: true,
     },
 
-    // 你也可以使用 Waline
+    // 如果你不需要评论，可以直接删除 comment 配置，
+    // 以下配置仅供体验，如果你需要评论，请自行配置并使用自己的环境，详见文档。
+    // 为了避免打扰主题开发者以及消耗他的资源，请不要在你的正式环境中直接使用下列配置!!!!!
     comment: {
+      /**
+       * Using giscus
+       */
       type: "giscus",
       repo: "vuepress-theme-hope/giscus-discussions",
       repoId: "R_kgDOG_Pt2A",
       category: "Announcements",
       categoryId: "DIC_kwDOG_Pt2M4COD69",
+
+      /**
+       * Using twikoo
+       */
+      // type: "twikoo",
+      // envId: "https://twikoo.ccknbc.vercel.app",
+
+      /**
+       * Using Waline
+       */
+      // type: "waline",
+      // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
     },
 
     mdEnhance: {
