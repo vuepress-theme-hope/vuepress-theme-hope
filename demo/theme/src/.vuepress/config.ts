a1b867c7c0a25a4e32482b2329f91bf49252db8a
Mr.Hope <zhangbowang1998@gmail.com>
2022-04-04 01:10:00 +0800

    chore(demo): add custom media

--- demo/theme/src/.vuepress/config.ts
+++ demo/theme/src/.vuepress/config.ts
@@ -18,18 +18,18 @@ export default defineHopeConfig({
     ],
   ],
 
-  // locales: {
-  //   "/": {
-  //     lang: "en-US",
-  //     title: "Theme Demo",
-  //     description: "A demo for vuepress-theme-hope",
-  //   },
-  //   "/zh/": {
-  //     lang: "zh-CN",
-  //     title: "主题演示",
-  //     description: "vuepress-theme-hope 的演示",
-  //   },
-  // },
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Theme Demo",
+      description: "A demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "主题演示",
+      description: "vuepress-theme-hope 的演示",
+    },
+  },
 
   themeConfig,
 });
