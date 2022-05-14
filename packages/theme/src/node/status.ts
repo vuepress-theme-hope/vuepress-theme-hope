c75031f9302cffbaad466494232e07b9d0d1efc3
Mr.Hope <mister-hope@outlook.com>
2022-05-15 00:47:56 +0800

    chore: tweaks

--- packages/theme/src/node/status.ts
+++ packages/theme/src/node/status.ts
@@ -11,14 +11,14 @@ export const getStatus = (themeOptions: HopeThemeOptions): ThemeStatus => {
   const { plugins = {} } = themeOptions;
 
   return {
-    enableBlog: Boolean(plugins?.blog),
+    enableBlog: Boolean(plugins.blog),
     enableEncrypt: Boolean(
       themeOptions.encrypt &&
         ("admin" in themeOptions.encrypt || "config" in themeOptions.encrypt)
     ),
     enableSlide: Boolean(
       plugins.mdEnhance &&
-        (plugins.mdEnhance?.enableAll || plugins.mdEnhance?.presentation)
+        (plugins.mdEnhance.enableAll || plugins.mdEnhance.presentation)
     ),
     enableVisitor: Boolean(
       plugins.comment && plugins.comment.type === "waline"
