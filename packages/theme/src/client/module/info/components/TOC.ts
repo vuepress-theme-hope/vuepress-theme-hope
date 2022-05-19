31213aeca3f949e3af85287d78c537271ae56656
Mr.Hope <mister-hope@outlook.com>
2022-05-19 18:53:48 +0800

    fix(theme): fix TOC scroll issue

--- packages/theme/src/client/module/info/components/TOC.ts
+++ packages/theme/src/client/module/info/components/TOC.ts
@@ -30,7 +30,7 @@ const renderChildren = (
     ? h(
         "ul",
         { class: "toc-list" },
-        headers.map((header: PageHeader) => [
+        headers.map((header) => [
           h(
             "li",
             {
@@ -39,7 +39,7 @@ const renderChildren = (
                 { active: isActiveLink(route, `#${header.slug}`) },
               ],
             },
-            [renderHeader(header)]
+            renderHeader(header)
           ),
           renderChildren(header.children, headerDepth - 1),
         ])
@@ -68,57 +68,68 @@ export default defineComponent({
     const metaLocale = useMetaLocale();
     const toc = ref<HTMLElement | null>(null);
 
+    const scrollTo = (top: number): void => {
+      toc.value?.scrollTo({ top, behavior: "smooth" });
+    };
+
     onMounted(() => {
       // scroll to active toc item
       watch(
         () => route.hash,
         (hash): void => {
-          // get the active toc item DOM, whose href equals to the current route
-          const activeTocItem = document.querySelector(
-            `#toc-list a.toc-link[href="${route.path}${hash}"]`
-          );
+          if (toc.value) {
+            // get the active toc item DOM, whose href equals to the current route
+            const activeTocItem = document.querySelector(
+              `#toc a.toc-link[href$="${hash}"]`
+            );
 
-          if (!activeTocItem) return;
+            if (!activeTocItem) return;
 
-          // get the top and height of the toc
-          const { top: tocTop, height: tocHeight } =
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            toc.value!.getBoundingClientRect();
-          // get the top and height of the active toc item
-          const { top: activeTocItemTop, height: activeTocItemHeight } =
-            activeTocItem.getBoundingClientRect();
+            // get the top and height of the toc
+            const { top: tocTop, height: tocHeight } =
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              toc.value.getBoundingClientRect();
+            // get the top and height of the active toc item
+            const { top: activeTocItemTop, height: activeTocItemHeight } =
+              activeTocItem.getBoundingClientRect();
 
-          // when the active toc item overflows the top edge of toc
-          if (activeTocItemTop < tocTop)
-            // scroll to the top edge of toc
-            activeTocItem.scrollIntoView(true);
-          // when the active toc item overflows the bottom edge of toc
-          else if (activeTocItemTop + activeTocItemHeight > tocTop + tocHeight)
-            // scroll to the bottom edge of toc
-            activeTocItem.scrollIntoView(false);
+            // when the active toc item overflows the top edge of toc
+            if (activeTocItemTop < tocTop)
+              // scroll to the top edge of toc
+              scrollTo(toc.value.scrollTop + activeTocItemTop - tocTop);
+            // when the active toc item overflows the bottom edge of toc
+            else if (
+              activeTocItemTop + activeTocItemHeight >
+              tocTop + tocHeight
+            )
+              // scroll to the bottom edge of toc
+              scrollTo(
+                toc.value.scrollTop +
+                  activeTocItemTop +
+                  activeTocItemHeight -
+                  tocTop -
+                  tocHeight
+              );
+          }
         }
       );
     });
 
-    return (): VNode => {
+    return (): VNode | null => {
       const tocHeaders = props.items.length
         ? renderChildren(props.items, props.headerDepth)
         : page.value.headers
         ? renderChildren(page.value.headers, props.headerDepth)
         : null;
 
-      return h("div", { class: "toc-place-holder", ref: toc }, [
-        h(
-          "aside",
-          { id: "toc-list" },
-          tocHeaders
-            ? [
-                h("div", { class: "toc-header" }, metaLocale.value.toc),
-                h("div", { class: "toc-wrapper" }, [tocHeaders]),
-              ]
-            : []
-        ),
-      ]);
+      return tocHeaders
+        ? h("div", { class: "toc-place-holder" }, [
+            h("aside", { id: "toc" }, [
+              h("div", { class: "toc-header" }, metaLocale.value.toc),
+              h("div", { class: "toc-wrapper", ref: toc }, [tocHeaders]),
+            ]),
+          ])
+        : null;
     };
   },
 });
