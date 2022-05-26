56ad4c333315a8738f4eb3626214f9a0507234fb
Zhengqbbb <1074059947@qq.com>
2022-05-26 13:45:06 +0800

    style: update quotes lint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,6 +1,6 @@
-const { execSync } = require('child_process')
 const fs = require("fs");
 const path = require("path");
+const { execSync } = require("child_process");
 
 const packages = fs.readdirSync(path.resolve(__dirname, "packages"));
 
