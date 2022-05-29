6a54314ebcc842695b0bef95477163381a1ea74a
Mr.Hope <mister-hope@outlook.com>
2022-05-30 01:45:31 +0800

    fix(lightgallery): fix lightgallery plugin

--- packages/lightgallery/src/client/config.ts
+++ packages/lightgallery/src/client/config.ts
@@ -2,7 +2,5 @@ import { defineClientConfig } from "@vuepress/client";
 import LightGallery from "./components/LightGallery";
 
 export default defineClientConfig({
-  enhance: ({ app }) => {
-    app.component("LightGallery", LightGallery);
-  },
+  rootComponents: [LightGallery],
 });
