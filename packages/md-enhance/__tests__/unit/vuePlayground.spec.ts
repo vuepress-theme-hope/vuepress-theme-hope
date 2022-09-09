072ca1a53db5f9c8a259750c397c07d77b84d064
Mr.Hope <mister-hope@outlook.com>
2022-09-09 13:51:48 +0800

    test(md-enhance): fix test file

--- packages/md-enhance/__tests__/unit/vuePlayground.spec.ts
+++ packages/md-enhance/__tests__/unit/vuePlayground.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import MarkdownIt from "markdown-it";
 
-import { vuePlayground } from "../../src/node/markdown-it";
+import { vuePlayground } from "../../src/node/markdown-it/index.js";
 
 describe("Vue Playground", () => {
   const markdownIt = MarkdownIt({ linkify: true }).use(vuePlayground);
