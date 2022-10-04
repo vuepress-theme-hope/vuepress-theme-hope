3fdd3e3820a2baae8926f10f6f1b1346523d048c
唐亮 <tang97155@163.com>
2022-10-04 20:36:35 +0800

    feat: Add ja support.

--- packages/theme/src/node/locales/index.ts
+++ packages/theme/src/node/locales/index.ts
@@ -9,6 +9,7 @@ import { ukLocale } from "./uk.js";
 import { viLocale } from "./vi.js";
 import { zhLocale } from "./zh.js";
 import { zhTWLocale } from "./zhTW.js";
+import { jpLocale } from "./jp.js";
 
 import type { HopeThemeLocaleData } from "../../shared/index.js";
 
@@ -34,4 +35,6 @@ export const themeLocalesData: Record<string, HopeThemeLocaleData> = {
   "/fr/": frLocale,
 
   "/es/": esLocale,
+
+  "/jp/": jpLocale,
 };
