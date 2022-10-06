bd4f10dd5b9ed03be87c84b970fbfd1a34bf7e5f
Mr.Hope <mister-hope@outlook.com>
2022-10-07 00:32:39 +0800

    feat(theme): add giscus custom theme

--- packages/theme/src/node/plugins/comment.ts
+++ packages/theme/src/node/plugins/comment.ts
@@ -12,6 +12,14 @@ export const getCommentPlugin = (
   return commentPlugin(
     <CommentOptions>{
       provider: "None",
+      ...(options?.provider === "Giscus"
+        ? {
+            lightTheme:
+              "https://cdn.githubraw.com/vuepress-theme-hope/vuepress-theme-hope/main/packages/theme/templates/giscus/light.css",
+            darkTheme:
+              "https://cdn.githubraw.com/vuepress-theme-hope/vuepress-theme-hope/main/packages/theme/templates/giscus/dark.css",
+          }
+        : {}),
       ...(options?.provider === "Waline"
         ? { dark: 'html[data-theme="dark"]' }
         : {}),
