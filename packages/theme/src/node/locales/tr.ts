d33c9aad5414e3ad7b11f4d4f5777db87718f6f4
Nihat Gurmen <mng@dexalot.com>
2022-11-02 08:20:33 -0500

    Update packages/theme/src/node/locales/tr.ts
    Co-authored-by: Mr.Hope <mister-hope@outlook.com>

--- packages/theme/src/node/locales/tr.ts
+++ packages/theme/src/node/locales/tr.ts
@@ -61,7 +61,6 @@ export const trLocale: HopeThemeLocaleData = {
   },
 
   routeLocales: {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     notFoundMsg: [
       "Burada bir şey yok.",
       "Buraya nasıl geldik?",
