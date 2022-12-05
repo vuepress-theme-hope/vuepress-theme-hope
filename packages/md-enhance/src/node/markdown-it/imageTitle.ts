3e003a6c3df64e727512315eb7fef21a19ac9ac4
Mr.Hope <mister-hope@outlook.com>
2022-12-05 15:35:26 +0800

    fix(md-enhance): fix imageTitle

--- packages/md-enhance/src/node/markdown-it/imageTitle.ts
+++ packages/md-enhance/src/node/markdown-it/imageTitle.ts
@@ -1,28 +1,11 @@
 import type { PluginSimple } from "markdown-it";
 
-export const imageTitle: PluginSimple = (
-  md,
-  {
-    light = ["gh-light-mode-only", "light"],
-    dark = ["gh-dark-mode-only", "dark"],
-  } = {}
-): void => {
+export const imageTitle: PluginSimple = (md): void => {
   const originalImageRender = md.renderer.rules.image!;
 
   md.renderer.rules.image = (tokens, index, options, env, self): string => {
     const token = tokens[index];
-    const src = token.attrGet("src");
     const title = token.attrGet("title");
-
-    if (src) {
-      if (light.some((item) => src.endsWith(`#${item}`))) {
-        token.attrSet("data-mode", "lightmode-only");
-        token.attrSet("src", src.replace(/#.*?$/, ""));
-      } else if (dark.some((item) => src.endsWith(`#${item}`))) {
-        token.attrSet("data-mode", "darkmode-only");
-        token.attrSet("src", src.replace(/#.*?$/, ""));
-      }
-    }
 
     const result = originalImageRender(tokens, index, options, env, self);
 
