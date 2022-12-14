e0928498c33c470f8e5af77bc2d92004d7286b30
Mr.Hope <mister-hope@outlook.com>
2022-12-14 14:17:01 +0800

    chore: bump to vp2beta59

--- packages/create/src/packageJson.ts
+++ packages/create/src/packageJson.ts
@@ -28,9 +28,9 @@ export const createPackageJson = async (
   const packageJsonPath = resolve(cwd, "package.json");
   const scripts = getScript(source);
   const devDependencies = {
-    "@vuepress/client": "2.0.0-beta.58",
+    "@vuepress/client": "2.0.0-beta.59",
     vue: "^3.2.45",
-    vuepress: "2.0.0-beta.58",
+    vuepress: "2.0.0-beta.59",
     "vuepress-theme-hope": version,
   };
 
