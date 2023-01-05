3338c6888d984df94f0a652f06451c9cd0be8ad9
Mr.Hope <mister-hope@outlook.com>
2023-01-05 11:06:14 +0800

    fix(remove-pwa): make options optional

--- packages/remove-pwa/src/node/plugin.ts
+++ packages/remove-pwa/src/node/plugin.ts
@@ -3,7 +3,9 @@ import { generateEmptyServiceWorker } from "./serviceWorker.js";
 import type { PluginObject } from "@vuepress/core";
 import type { RemovePWAOptions } from "./options.js";
 
-export const removePWAPlugin = (options: RemovePWAOptions): PluginObject => ({
+export const removePWAPlugin = (
+  options: RemovePWAOptions = {}
+): PluginObject => ({
   name: "vuepress-plugin-remove-pwa",
 
   onGenerated: (app): Promise<void> => generateEmptyServiceWorker(app, options),
