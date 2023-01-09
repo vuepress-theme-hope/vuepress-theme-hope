ea860b03a4b886e85b7a546c58570b4c1c3ae040
Mr.Hope <mister-hope@outlook.com>
2023-01-10 00:42:57 +0800

    docs: fix docsearch with patch

--- docs-shared/src/config-wrapper.ts
+++ docs-shared/src/config-wrapper.ts
@@ -114,6 +114,7 @@ export const config = (
               appId: "VXIEHELDL1",
               apiKey: "595796f71b6ba14326719682c3738c0c",
               indexName: docsearchIndexName,
+              indexBase: "/v2/",
               locales: {
                 "/zh/": {
                   placeholder: "搜索文档",
