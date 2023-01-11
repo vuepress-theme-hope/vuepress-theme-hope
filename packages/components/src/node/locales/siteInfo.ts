98b975137366db50e70074223ab43df02ac2d889
Mr.Hope <mister-hope@outlook.com>
2023-01-11 17:26:28 +0800

    chore: update locales

--- packages/components/src/node/locales/siteInfo.ts
+++ packages/components/src/node/locales/siteInfo.ts
@@ -7,5 +7,29 @@ export const siteInfoLocaleConfig: SiteInfoLocaleConfig = {
 
   "/zh-tw/": { source: "源代碼" },
 
+  "/de-at/": { source: "Quellcode" },
+
+  "/vi/": { source: "Mã nguồn" },
+
+  "/uk/": { source: "Джерело" },
+
   "/ru/": { source: "Исходный код" },
+
+  "/br/": { source: "Código fonte" },
+
+  "/pl/": { source: "Źródło" },
+
+  "/sk/": { source: "Zdrojový kód" },
+
+  "/fr/": { source: "Code source" },
+
+  "/es/": { source: "Código fuente" },
+
+  "/ja/": { source: "ソースコード" },
+
+  "/tr/": { source: "Kaynak kodu" },
+
+  "/ko/": { source: "소스 코드" },
+
+  "/fi/": { source: "Lähdekoodi" },
 };
