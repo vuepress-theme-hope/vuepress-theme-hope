29969589fcc82078f15bf07b88cd6e6d2bcf07f4
Mr.Hope <mister-hope@outlook.com>
2023-01-11 17:52:25 +0800

    fix(theme): fix pure default value

--- packages/theme/src/client/composables/themeData.ts
+++ packages/theme/src/client/composables/themeData.ts
@@ -25,4 +25,4 @@ export const useThemeAuthor = (): ComputedRef<AuthorInfo[]> => {
 };
 
 export const usePure = (): ComputedRef<boolean> =>
-  computed(() => useThemeData().value.pure !== false);
+  computed(() => Boolean(useThemeData().value.pure));
