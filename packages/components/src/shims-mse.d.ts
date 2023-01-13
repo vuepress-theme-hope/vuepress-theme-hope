0f53c986c3688f41644f059ff2ccb9aa1cb1f181
Mr.Hope <mister-hope@outlook.com>
2023-01-13 20:18:46 +0800

    chore: fix types

--- packages/components/src/shims-mse.d.ts
+++ packages/components/src/shims-mse.d.ts
@@ -4,13 +4,11 @@ declare module "dashjs/dist/dash.all.min.js" {
 }
 declare module "hls.js/dist/hls.min.js" {
   import HLS from "hls.js";
-
-  export * from "hls.js";
-  export default HLS;
+  export = HLS;
 }
 
 declare module "mpegts.js/dist/mpegts.js" {
   import mpegts from "mpegts.js";
-  export * from "mpegts.js";
-  export default mpegts;
+
+  export = mpegts;
 }
