77a28bd0ddc5a96046eef690aa772706305a779a
Mr.Hope <mister-hope@outlook.com>
2023-02-17 22:54:02 +0800

    chore: tweaks

--- scripts/rollup.ts
+++ scripts/rollup.ts
@@ -133,7 +133,7 @@ export const rollupTypescript = (
               : []),
             ...dtsExternal,
           ],
-        } satisfies RollupOptions,
+        } as RollupOptions,
       ]
     : []),
 ];
