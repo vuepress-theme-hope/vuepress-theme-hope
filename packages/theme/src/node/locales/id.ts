38fd753f80c11d28da85532beac3f9bb0babdcee
agisnrynt <agisnuryanto@outlook.co.id>
2023-02-26 09:31:12 +0700

    add indonesian translation

---
+++ packages/theme/src/node/locales/id.ts
@@ -0,0 +1,74 @@
+import { type ThemeLocaleData } from "../../shared/index.js";
+
+export const enLocale: ThemeLocaleData = {
+  lang: "id-ID",
+
+  navbarLocales: {
+    langName: "Indonesia",
+    selectLangAriaLabel: "Pilih Bahasa",
+  },
+
+  metaLocales: {
+    author: "Penulis",
+    date: "Ditulis pada",
+    origin: "Original",
+    views: "Dilihat",
+    category: "Kategori",
+    tag: "Tag",
+    readingTime: "Waktu Membaca",
+    words: "Kata",
+    toc: "Pada Halaman Ini",
+    prev: "Sebelumnya",
+    next: "Selanjutnya",
+    lastUpdated: "Terakhir diperbarui",
+    contributors: "Kontributor",
+    editLink: "Edit halaman ini",
+  },
+
+  blogLocales: {
+    article: "Artikel",
+    articleList: "Daftar artikel",
+    category: "Kategori",
+    tag: "Tag",
+    timeline: "Linimasa",
+    timelineTitle: "Kemarin adalah kenangan, esok adalah harapan",
+    all: "Semua",
+    intro: "Perkenalan",
+    star: "Berbintang",
+  },
+
+  paginationLocales: {
+    prev: "Sebelumnya",
+    next: "Selanjutnya",
+    navigate: "Ke Halaman",
+    action: "Pergi",
+    errorText: "Tolong masukkan angka antara 1 dan $page !",
+  },
+
+  outlookLocales: {
+    themeColor: "Warna Tema",
+    darkmode: "Mode Tema",
+    fullscreen: "Layar Penuh",
+  },
+
+  encryptLocales: {
+    iconLabel: "Halaman terkunci",
+    placeholder: "Masukkan kata sandi",
+    remember: "Ingat kata sandi",
+    errorHint: "Mohon masukkan kata sandi yang benar!",
+  },
+
+  routeLocales: {
+    skipToContent: "Langsung ke konten utama",
+    notFoundTitle: "Halaman tidak ditemukan",
+    notFoundMsg: [
+      "Tidak ada apapun disini.",
+      "Bagaimana kita bisa sampai kesini?",
+      "Itu adalah Empat-Nol-Empat.",
+      "Tampaknya kami memiliki beberapa tautan situs yang rusak.",
+    ],
+    back: "Kembali",
+    home: "Ke Beranda",
+    openInNewWindow: "Buka di jendela baru",
+  },
+};
