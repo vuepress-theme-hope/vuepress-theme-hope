0556b6ff32d3bc020d6f6217e700cb97f9340f44
Mr.Hope <mister-hope@outlook.com>
2023-03-11 23:26:35 +0800

    fix(theme): fix alias

--- packages/theme/src/client/export.ts
+++ packages/theme/src/client/export.ts
@@ -4,6 +4,6 @@ export {
   injectDarkmode,
   setupDarkmode,
 } from "@theme-hope/modules/outlook/composables/index";
-export { setupSidebarItems } from "@theme-hope//modules/sidebar/composables/index";
+export { setupSidebarItems } from "@theme-hope/modules/sidebar/composables/index";
 export { default as Layout } from "@theme-hope/layouts/Layout";
 export { default as NotFound } from "@theme-hope/layouts/NotFound";
