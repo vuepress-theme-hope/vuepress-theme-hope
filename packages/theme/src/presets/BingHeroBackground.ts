502d0212b67b8517c1e0ef3569e2435d85b7771f
Mr.Hope <mister-hope@outlook.com>
2023-04-19 22:11:59 +0800

    chore: tweaks

--- packages/theme/src/presets/BingHeroBackground.ts
+++ packages/theme/src/presets/BingHeroBackground.ts
@@ -4,45 +4,40 @@ import { type VNode, computed, defineComponent, h, onMounted } from "vue";
 
 import "./bing-hero-background.scss";
 
-interface BingWallpaperData {
+interface BingWallpaperInfo {
   url: string;
-
-  title: string;
-  copyright: string;
-  copyrightLink: string;
-  path: string;
-  ssd: string;
+  wallpaper: string;
+  downloadable: boolean;
+  locales: Record<
+    string,
+    {
+      title: string;
+      description: string;
+      headline: string;
+      copyright: string;
+      backstage: string;
+      quickFact: string;
+    }
+  >;
 }
-
 const bingStorage = useStorage<{
   index: number;
-  data: Record<string, BingWallpaperData>[];
+  data: BingWallpaperInfo[];
 }>("bing-image", {
   index: 0,
   data: [],
 });
-
-interface Mo7BingWallpaperData {
-  Copyright: string;
-  CopyrightLink: string;
-  Path: string;
-  Ssd: string;
-  Title: string;
-  Url: string;
-}
-
-interface Mo7BingResponse {
-  Msg: "Succeed";
-
-  Code: 1;
-  Data: (Mo7BingWallpaperData & { EN: Mo7BingWallpaperData })[];
-}
 
 const LOCATION_SVG =
   '<svg aria-hidden="true" viewBox="0 0 12 12"><path d="M6.5 3A1.5 1.5 0 1 0 8 4.5 1.5 1.5 0 0 0 6.5 3zm0-3A4.5 4.5 0 0 0 2 4.5a5.607 5.607 0 0 0 .087.873c.453 2.892 2.951 5.579 3.706 6.334a1 1 0 0 0 1.414 0c.755-.755 3.253-3.442 3.706-6.334A5.549 5.549 0 0 0 11 4.5 4.5 4.5 0 0 0 6.5 0zm3.425 5.218C9.565 7.514 7.632 9.868 6.5 11 5.369 9.868 3.435 7.514 3.075 5.218A4.694 4.694 0 0 1 3 4.5a3.5 3.5 0 0 1 7 0 4.634 4.634 0 0 1-.075.718z"/></svg>';
 
 export default defineComponent({
   name: "BingHeroBackground",
+
+  // TODO: Add download button, image description and copyright information
+  // props: {
+  //   download: Boolean,
+  // },
 
   setup() {
     const lang = usePageLang();
@@ -51,41 +46,25 @@ export default defineComponent({
       const info = bingStorage.value.data[bingStorage.value.index];
       const langCode = lang.value.toLowerCase().split("-").pop();
 
-      if (info) return info[langCode!] ?? info["en"];
+      if (info) {
+        const { url, wallpaper, downloadable, locales } = info;
+
+        return {
+          url,
+          wallpaper,
+          downloadable,
+          ...(locales[langCode!] ?? locales["en"]),
+        };
+      }
 
       return null;
     });
 
-    const getWallpapers = (
-      response: Mo7BingResponse
-    ): Record<string, BingWallpaperData>[] =>
-      response.Data.map(({ EN, ...ZH }) => {
-        return {
-          zh: {
-            copyright: ZH.Copyright,
-            copyrightLink: ZH.CopyrightLink,
-            path: ZH.Path,
-            ssd: ZH.Ssd,
-            title: ZH.Title,
-            url: ZH.Url,
-          },
-          en: {
-            copyright: EN.Copyright,
-            copyrightLink: EN.CopyrightLink,
-            path: EN.Path,
-            ssd: EN.Ssd,
-            title: EN.Title,
-            url: EN.Url,
-          },
-        };
-      });
-
-    const getImage = (): Promise<void> =>
-      fetch("//file.mo7.cc/api/public/url")
-        .then((response) => <Promise<Mo7BingResponse>>response.json())
-        .then((res) => {
-          bingStorage.value.data = getWallpapers(res);
-        });
+    const getImage = (): Promise<BingWallpaperInfo[]> => {
+      return fetch("//bing-wallpaper.vuejs.press/api/wallpaper").then(
+        (response) => <Promise<BingWallpaperInfo[]>>response.json()
+      );
+    };
 
     const prev = (): void => {
       bingStorage.value.index--;
@@ -96,7 +75,9 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      void getImage();
+      void getImage().then((res) => {
+        bingStorage.value.data = res;
+      });
     });
 
     return (): VNode[] | null =>
@@ -113,7 +94,7 @@ export default defineComponent({
                 "a",
                 {
                   class: "bing-info",
-                  href: currentWallpaper.value.copyrightLink,
+                  href: currentWallpaper.value.backstage,
                   target: "_blank",
                 },
                 [
