05096b5cc0ef0c4d51647899801122acdf7ce12c
Mr.Hope <mister-hope@outlook.com>
2023-04-28 23:08:24 +0800

    docs(theme): add flowchart playground

--- docs/theme/src/.vuepress/config.ts
+++ docs/theme/src/.vuepress/config.ts
@@ -43,6 +43,10 @@ export default config(
     ],
 
     alias: {
+      "@FlowChartPlayground": path.resolve(
+        __dirname,
+        "./components/FlowChartPlayground"
+      ),
       "@IconDisplay": path.resolve(__dirname, "./components/IconDisplay"),
       "@KatexPlayground": path.resolve(
         __dirname,
