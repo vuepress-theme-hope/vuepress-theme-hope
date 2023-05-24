48f888cc788886842635057bd5ad25290b14d7cf
Mr.Hope <mister-hope@outlook.com>
2023-05-24 13:42:59 +0800

    test: improve coverage

---
+++ packages/shared/__tests__/unit/checkVersion.spec.ts
@@ -0,0 +1,57 @@
+import { createBaseApp } from "@vuepress/core";
+import { getDirname, path } from "@vuepress/utils";
+import { describe, expect, it } from "vitest";
+
+import { emptyTheme } from "./__fixtures__/theme/empty.js";
+import { checkVersion } from "../../src/node/utils/checkVersion.js";
+
+const __dirname = getDirname(import.meta.url);
+
+const fixtures = path.resolve(__dirname, "./__fixtures__/check-version");
+
+it("Should return true", () => {
+  const app1 = createBaseApp({
+    source: path.resolve(fixtures, "correct-version1"),
+
+    bundler: {} as any,
+    theme: emptyTheme,
+  });
+  const app2 = createBaseApp({
+    source: path.resolve(fixtures, "correct-version2"),
+
+    bundler: {} as any,
+    theme: emptyTheme,
+  });
+
+  expect(checkVersion(app1, "test", "2.0.0-beta.62")).toEqual(true);
+  expect(checkVersion(app2, "test", "2.0.0-beta.62")).toEqual(true);
+});
+
+it("Should return false", () => {
+  const app1 = createBaseApp({
+    source: path.resolve(fixtures, "wrong-version1"),
+    bundler: {} as any,
+    theme: emptyTheme,
+  });
+  const app2 = createBaseApp({
+    source: path.resolve(fixtures, "wrong-version2"),
+    bundler: {} as any,
+    theme: emptyTheme,
+  });
+
+  expect(checkVersion(app1, "wrong1", "2.0.0-beta.61")).toEqual(false);
+  expect(checkVersion(app2, "wrong2", "2.0.0-beta.61")).toEqual(false);
+});
+
+describe("Should support nesting", () => {
+  it("Should return true", () => {
+    const app1 = createBaseApp({
+      source: path.resolve(fixtures, "nesting/sub-folder"),
+
+      bundler: {} as any,
+      theme: emptyTheme,
+    });
+
+    expect(checkVersion(app1, "nesting", "2.0.0-beta.62")).toEqual(true);
+  });
+});
---
+++ packages/shared/__tests__/unit/packageManager.spec.ts
@@ -0,0 +1,36 @@
+import { getDirname, path } from "@vuepress/utils";
+import { describe, expect, it } from "vitest";
+
+import {
+  detectPackageManager,
+  hasGlobalInstallation,
+} from "../../src/node/utils/packageManager.js";
+
+const __dirname = getDirname(import.meta.url);
+
+const fixtures = path.resolve(__dirname, "./__fixtures__/package-manager");
+
+it("Should detect global package manager", () => {
+  expect(hasGlobalInstallation("npm")).toBeTruthy();
+  expect(hasGlobalInstallation("pnpm")).toBeTruthy();
+});
+
+describe("Should detect lockfile", () => {
+  it("Should be npm", () => {
+    expect(
+      detectPackageManager(path.resolve(fixtures, "lock-file/npm"))
+    ).toEqual("npm");
+  });
+
+  it("Should be yarn", () => {
+    expect(
+      detectPackageManager(path.resolve(fixtures, "lock-file/yarn"))
+    ).toEqual("yarn");
+  });
+
+  it("Should be pnpm", () => {
+    expect(
+      detectPackageManager(path.resolve(fixtures, "lock-file/pnpm"))
+    ).toEqual("pnpm");
+  });
+});
