16b03503b84d3042f78f015cb26748179367af4f
Mr.Hope <mister-hope@outlook.com>
2023-05-31 09:13:45 +0800

    fix(lightgallery): fix client config inject

--- packages/lightgallery/src/client/config.ts
+++ packages/lightgallery/src/client/config.ts
@@ -1,7 +1,11 @@
 import { defineClientConfig } from "@vuepress/client";
 
 import LightGallery from "./components/LightGallery.js";
+import { injectLightGalleryConfig } from "./helpers/index.js";
 
 export default defineClientConfig({
+  enhance: ({ app }) => {
+    injectLightGalleryConfig(app);
+  },
   rootComponents: [LightGallery],
 });
