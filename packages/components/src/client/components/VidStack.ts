8f7ce792db726f0270930278c6a51269c4b75171
Mr.Hope <mister-hope@outlook.com>
2023-06-08 11:50:58 +0800

    chore(components): update style id

--- packages/components/src/client/components/VidStack.ts
+++ packages/components/src/client/components/VidStack.ts
@@ -80,7 +80,7 @@ export default defineComponent({
       ]
         .map((url) => `@import url("${url}");`)
         .join("\n"),
-      { id: "vidstack" }
+      { id: "vidstack-style" }
     );
 
     onMounted(() => defineCustomElements());
