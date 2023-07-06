4347ebb1b01b287f1e02a0d38464513a0d643d7f
Mr.Hope <mister-hope@outlook.com>
2023-07-06 16:20:00 +0800

    chore: update demo

--- demo/pwa2/src/.vuepress/config.ts
+++ demo/pwa2/src/.vuepress/config.ts
@@ -5,7 +5,6 @@ import { pwaPlugin } from "vuepress-plugin-pwa2";
 export default defineUserConfig({
   base: "/",
 
-  lang: "zh-CN",
   title: "PWA Plugin",
   description: "PWA Plugin for VuePress2",
 
@@ -15,7 +14,28 @@ export default defineUserConfig({
     repo: "vuepress-theme-hope/vuepress-theme-hope/tree/main/demo/pwa2/",
   }),
 
-  plugins: [pwaPlugin()],
+  plugins: [
+    pwaPlugin({
+      manifest: {
+        name: "vuepress-plugin-pwa2 ",
+        short_name: "pwa2 demo",
+        description: "PWA plugin demo for VuePress",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/img/icon/chrome192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/img/icon/chrome512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 
   shouldPrefetch: false,
 });
