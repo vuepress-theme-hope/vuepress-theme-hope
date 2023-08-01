ca6cd86b62ed7bf3b06fec79a4679dc633862695
Mr.Hope <mister-hope@outlook.com>
2023-08-01 18:55:18 +0800

    fix(create): fix template

--- packages/create/src/packageJson.ts
+++ packages/create/src/packageJson.ts
@@ -38,7 +38,7 @@ export const createPackageJson = async (
   const scripts = getScript(packageManager, source);
   const devDependencies = {
     "@vuepress/client": "2.0.0-beta.66",
-    vue: "^3.2.47",
+    vue: "^3.3.4",
     vuepress: "2.0.0-beta.66",
     "vuepress-theme-hope": version,
   };
