fd302ef9336ffd87a579e2907243716994857510
Mr.Hope <mister-hope@outlook.com>
2023-10-16 23:02:04 +0800

    feat(md-enhance): mark deps optional

--- packages/md-enhance/src/node/utils.ts
+++ packages/md-enhance/src/node/utils.ts
@@ -1,7 +1,10 @@
+import { createRequire } from "node:module";
+
 import { getDirname, path } from "@vuepress/utils";
 import { Logger, ensureEndingSlash } from "vuepress-shared/node";
 
 const __dirname = getDirname(import.meta.url);
+const require = createRequire(import.meta.url);
 
 export const PLUGIN_NAME = "vuepress-plugin-md-enhance";
 
@@ -10,3 +13,15 @@ export const logger = new Logger(PLUGIN_NAME);
 export const CLIENT_FOLDER = ensureEndingSlash(
   path.resolve(__dirname, "../client"),
 );
+
+export const isInstalled = (pkg: string, hint = true): boolean => {
+  try {
+    require.resolve(pkg);
+
+    return true;
+  } catch (error) {
+    if (hint) logger.error(`Package ${pkg} is not installed.`);
+
+    return false;
+  }
+};
