ec1feea0b747a3211214a9b89ea31b6ed48d17bd
Mr.Hope <mister-hope@outlook.com>
2023-11-26 15:01:28 +0800

    feat(theme): support item prop in project-panel, close #2562

--- packages/theme/src/client/modules/blog/components/BlogHome.ts
+++ packages/theme/src/client/modules/blog/components/BlogHome.ts
@@ -1,5 +1,6 @@
+import { usePageFrontmatter } from "@vuepress/client";
 import type { VNode } from "vue";
-import { defineComponent, h } from "vue";
+import { computed, defineComponent, h } from "vue";
 
 import MarkdownContent from "@theme-hope/components/MarkdownContent";
 import DropTransition from "@theme-hope/components/transitions/DropTransition";
@@ -9,6 +10,8 @@ import InfoPanel from "@theme-hope/modules/blog/components/InfoPanel";
 import ProjectPanel from "@theme-hope/modules/blog/components/ProjectPanel";
 import { useArticles } from "@theme-hope/modules/blog/composables/index";
 
+import type { ThemeBlogHomePageFrontmatter } from "../../../../shared/index.js";
+
 import "../styles/home.scss";
 
 export default defineComponent({
@@ -16,15 +19,20 @@ export default defineComponent({
 
   setup() {
     const articles = useArticles();
+    const frontmatter = usePageFrontmatter<ThemeBlogHomePageFrontmatter>();
+
+    const projects = computed(() => frontmatter.value.projects ?? []);
 
     return (): VNode =>
       h("div", { class: "vp-page vp-blog" }, [
         h(BlogHero),
         h("div", { class: "blog-page-wrapper" }, [
           h("main", { id: "main-content", class: "vp-blog-main" }, [
-            h(DropTransition, { appear: true, delay: 0.16 }, () =>
-              h(ProjectPanel),
-            ),
+            projects.value.length
+              ? h(DropTransition, { appear: true, delay: 0.16 }, () =>
+                  h(ProjectPanel, { items: projects.value }),
+                )
+              : null,
             h(DropTransition, { appear: true, delay: 0.24 }, () =>
               h(ArticleList, { items: articles.value.items }),
             ),
