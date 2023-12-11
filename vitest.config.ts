f032895c57d2af88949643e30d7cbabe8b399972
Mr.Hope <mister-hope@outlook.com>
2023-12-11 13:37:30 +0800

    chore: update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      include: ["packages/*/src/**/*.ts"],
+    },
     include: ["**/*.spec.ts"],
   },
 });
