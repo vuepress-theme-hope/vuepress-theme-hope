c120670864123cbd6da959eee3f133bc16d8e4e5
Mr.Hope <mister-hope@outlook.com>
2024-01-26 04:37:09 +0800

    feat(theme): improve HopeIcon

--- packages/theme/src/client/components/HopeIcon.ts
+++ packages/theme/src/client/components/HopeIcon.ts
@@ -12,18 +12,25 @@ export interface IconProps {
 
 const HopeIcon: FunctionalComponent<IconProps> = (props) => {
   const { icon = "", color, size } = props;
-  const style: Record<string, string> = {};
+  const style = color || size ? <Record<string, string>>{} : null;
 
-  if (color) style["color"] = color;
+  if (color) style!["color"] = color;
   if (size)
-    style["height"] = Number.isNaN(Number(size)) ? <string>size : `${size}px`;
+    style!["height"] = Number.isNaN(Number(size)) ? <string>size : `${size}px`;
 
   return isLinkHttp(icon)
-    ? h("img", { class: "icon", src: icon, "no-view": "", style })
+    ? h("img", {
+        class: "icon",
+        src: icon,
+        alt: "",
+        "no-view": "",
+        style,
+      })
     : isAbsoluteUrl(icon)
       ? h("img", {
           class: "icon",
           src: withBase(icon),
+          alt: "",
           "aria-hidden": "",
           "no-view": "",
           style,
