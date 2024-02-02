76d39297b1293d86091c1dfeb890febd8f785828
Mr.Hope <mister-hope@outlook.com>
2024-02-03 00:20:33 +0800

    chore(redirect): tweaks

--- packages/redirect/src/cli/index.ts
+++ packages/redirect/src/cli/index.ts
@@ -15,12 +15,23 @@ import { fs, logger, path } from "vuepress/utils";
 
 import { getRedirectHTML } from "../node/utils/index.js";
 
+interface RedirectCommandOptions {
+  hostname: string;
+  output?: string;
+  config?: string;
+  cache: string;
+  temp?: string;
+  cleanCache?: boolean;
+  cleanTemp?: boolean;
+}
+
 const require = createRequire(import.meta.url);
 
 const cli = cac("vp-redirect");
-const version = <
-  string // eslint-disable-next-line
->require("vuepress-plugin-redirect/package.json").version;
+const { version } = <{ version: string }>(
+  // eslint-disable-next-line import/no-commonjs
+  require("vuepress-plugin-redirect/package.json")
+);
 
 cli
   .command(
@@ -30,9 +41,7 @@ cli
   .option(
     "--hostname <hostname>",
     "Hostname to redirect to (E.g.: https://new.example.com/)",
-    {
-      default: "/",
-    },
+    { default: "/" },
   )
   .option("-c, --config <config>", "Set path to config file")
   .option(
@@ -43,95 +52,80 @@ cli
   .option("-t, --temp <temp>", "Set the directory of the temporary files")
   .option("--clean-cache", "Clean the cache files before generation")
   .option("--clean-temp", "Clean the temporary files before generation")
-  .action(
-    async (
-      sourceDir: string,
-      commandOptions: {
-        hostname: string;
-        output?: string;
-        config?: string;
-        cache: string;
-        temp?: string;
-        cleanCache?: boolean;
-        cleanTemp?: boolean;
-      },
-    ) => {
-      if (!sourceDir) return cli.outputHelp();
+  .action(async (sourceDir: string, commandOptions: RedirectCommandOptions) => {
+    if (!sourceDir) return cli.outputHelp();
 
-      if (process.env["NODE_ENV"] === undefined)
-        process.env["NODE_ENV"] = "production";
+    // ensure NODE_ENV is set
+    process.env["NODE_ENV"] ??= "production";
 
-      // resolve app config from cli options
-      const cliAppConfig = resolveCliAppConfig(sourceDir, {});
+    // resolve app config from cli options
+    const cliAppConfig = resolveCliAppConfig(sourceDir, {});
 
-      // resolve user config file
-      const userConfigPath = resolveUserConfigConventionalPath(
-        cliAppConfig.source,
-      );
+    // resolve user config file
+    const userConfigPath = resolveUserConfigConventionalPath(
+      cliAppConfig.source,
+    );
 
-      const { userConfig } = await loadUserConfig(userConfigPath);
+    const { userConfig } = await loadUserConfig(userConfigPath);
 
-      // resolve the final app config to use
-      const appConfig = resolveAppConfig({
-        defaultAppConfig: {},
-        cliAppConfig,
-        userConfig,
-      });
+    // resolve the final app config to use
+    const appConfig = resolveAppConfig({
+      defaultAppConfig: {},
+      cliAppConfig,
+      userConfig,
+    });
 
-      if (appConfig === null) return;
+    if (appConfig === null) return;
 
-      // create vuepress app
-      const app = createBuildApp(appConfig);
+    // create vuepress app
+    const app = createBuildApp(appConfig);
 
-      // use user-config plugin
-      app.use(transformUserConfigToPlugin(userConfig, cliAppConfig.source));
+    // use user-config plugin
+    app.use(transformUserConfigToPlugin(userConfig, cliAppConfig.source));
 
-      // clean temp and cache
-      if (commandOptions.cleanTemp === true) {
-        logger.info("Cleaning temp...");
-        await fs.remove(app.dir.temp());
-      }
-      if (commandOptions.cleanCache === true) {
-        logger.info("Cleaning cache...");
-        await fs.remove(app.dir.cache());
-      }
+    // clean temp and cache
+    if (commandOptions.cleanTemp === true) {
+      logger.info("Cleaning temp...");
+      await fs.remove(app.dir.temp());
+    }
+    if (commandOptions.cleanCache === true) {
+      logger.info("Cleaning cache...");
+      await fs.remove(app.dir.cache());
+    }
 
-      const outputFolder = commandOptions.output
-        ? path.join(process.cwd(), commandOptions.output)
-        : path.join(app.dir.source(), ".vuepress", "redirect");
+    const outputFolder = commandOptions.output
+      ? path.join(process.cwd(), commandOptions.output)
+      : path.join(app.dir.source(), ".vuepress", "redirect");
 
-      // empty output directory
-      await fs.emptyDir(outputFolder);
+    // empty output directory
+    await fs.emptyDir(outputFolder);
 
-      // initialize vuepress app to get pages
-      logger.info("Initializing VuePress and preparing data...");
+    // initialize vuepress app to get pages
+    logger.info("Initializing VuePress and preparing data...");
 
-      await app.init();
+    await app.init();
 
-      // redirect all pages
+    // redirect all pages
 
-      // initialize vuepress app to get pages
-      logger.info("Generating redirect pages...");
+    // initialize vuepress app to get pages
+    logger.info("Generating redirect pages...");
 
-      await Promise.all(
-        app.pages.map((page) => {
-          const redirectUrl = `${removeEndingSlash(commandOptions.hostname)}${
-            app.options.base
-          }${removeLeadingSlash(page.path)}`;
-          const destLocation = path.join(
-            outputFolder,
-            removeLeadingSlash(page.path.replace(/\/$/, "/index.html")),
-          );
+    await Promise.all(
+      app.pages.map((page) => {
+        const redirectUrl = `${removeEndingSlash(commandOptions.hostname)}${
+          app.options.base
+        }${removeLeadingSlash(page.path)}`;
+        const destLocation = path.join(
+          outputFolder,
+          removeLeadingSlash(page.path.replace(/\/$/, "/index.html")),
+        );
 
-          return fs
-            .ensureDir(path.dirname(destLocation))
-            .then(() =>
-              fs.writeFile(destLocation, getRedirectHTML(redirectUrl)),
-            );
-        }),
-      );
-    },
-  );
+        return fs
+          .ensureDir(path.dirname(destLocation))
+          .then(() => fs.writeFile(destLocation, getRedirectHTML(redirectUrl)));
+      }),
+    );
+  });
 
 cli.command("").action(() => cli.outputHelp());
 
