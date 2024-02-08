19e91e3187a9ffbac1c0d735a373a1e4c3468675
Mr.Hope <mister-hope@outlook.com>
2024-02-08 18:16:09 +0800

    chore: update deps

--- packages/create/src/packageJson.ts
+++ packages/create/src/packageJson.ts
@@ -57,7 +57,7 @@ export const createPackageJson = async ({
   const scripts = getScript(packageManager, bundler, source);
   const devDependencies = {
     [`@vuepress/bundler-${bundler}`]: "2.0.0-rc.7",
-    vue: "^3.4.15",
+    vue: "^3.4.16",
     vuepress: "2.0.0-rc.7",
     "vuepress-theme-hope": version,
   };
