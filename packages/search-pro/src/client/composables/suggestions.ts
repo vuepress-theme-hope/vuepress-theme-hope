cc31927f1ee3729fc4b6d90d52253ffebe823682
Mr.Hope <mister-hope@outlook.com>
2024-03-30 12:55:10 +0800

    feat(search-pro): improve search worker

--- packages/search-pro/src/client/composables/suggestions.ts
+++ packages/search-pro/src/client/composables/suggestions.ts
@@ -3,9 +3,9 @@ import type { Ref } from "vue";
 import { onMounted, onUnmounted, ref, watch } from "vue";
 import { usePageData, useRouteLocale } from "vuepress/client";
 
+import { createSearchWorker } from "../createSearchWorker.js";
 import { enableAutoSuggestions } from "../define.js";
 import { useSearchOptions } from "../helpers/index.js";
-import { createSearchWorker } from "../utils/index.js";
 
 export interface SuggestionsRef {
   suggestions: Ref<string[]>;
@@ -24,46 +24,44 @@ export const useSearchSuggestions = (
     onMounted(() => {
       const { suggest, terminate } = createSearchWorker();
 
-      watch(
-        [queries, routeLocale],
-        ([queries]) => {
-          const query = queries.join(" ");
-          const {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            searchFilter,
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            splitWord,
-            suggestionsFilter = (suggestions): string[] => suggestions,
-            ...options
-          } = searchOptions.value;
+      const performSuggestion = (queries: string[]): void => {
+        const query = queries.join(" ");
+        const {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          searchFilter,
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          splitWord,
+          suggestionsFilter = (suggestions): string[] => suggestions,
+          ...options
+        } = searchOptions.value;
 
-          if (query)
-            suggest(query, routeLocale.value, options)
-              .then((suggestions) =>
-                suggestionsFilter(
-                  suggestions,
-                  query,
-                  routeLocale.value,
-                  pageData.value,
-                ),
-              )
-              .then((_suggestions) => {
-                suggestions.value = _suggestions.length
-                  ? startsWith(_suggestions[0], query) &&
-                    !_suggestions[0].slice(query.length).includes(" ")
-                    ? _suggestions
-                    : [query, ..._suggestions]
-                  : [];
-              })
-              .catch((err) => {
-                console.error(err);
-              });
-          else suggestions.value = [];
-        },
-        {
-          immediate: true,
-        },
-      );
+        if (query)
+          suggest(query, routeLocale.value, options)
+            .then((suggestions) =>
+              suggestionsFilter(
+                suggestions,
+                query,
+                routeLocale.value,
+                pageData.value,
+              ),
+            )
+            .then((_suggestions) => {
+              suggestions.value = _suggestions.length
+                ? startsWith(_suggestions[0], query) &&
+                  !_suggestions[0].slice(query.length).includes(" ")
+                  ? _suggestions
+                  : [query, ..._suggestions]
+                : [];
+            })
+            .catch((err) => {
+              console.warn(err);
+            });
+        else suggestions.value = [];
+      };
+
+      watch([queries, routeLocale], ([queries]) => performSuggestion(queries), {
+        immediate: true,
+      });
 
       onUnmounted(() => {
         terminate();
