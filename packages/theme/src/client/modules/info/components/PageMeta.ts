6b890edd646b48c3f5da59f99782882171f75cbe
Mr.Hope <mister-hope@outlook.com>
2024-04-02 13:43:05 +0800

    chore(theme): update PageMeta class

--- packages/theme/src/client/modules/info/components/PageMeta.ts
+++ packages/theme/src/client/modules/info/components/PageMeta.ts
@@ -25,34 +25,46 @@ export default defineComponent({
     return (): VNode => {
       const { metaLocales } = themeLocale.value;
 
-      return h("footer", { class: "page-meta" }, [
+      return h("footer", { class: "vp-page-meta" }, [
         editLink.value
           ? h(
               "div",
-              { class: "meta-item edit-link" },
+              { class: "vp-meta-item edit-link" },
               h(
                 AutoLink,
-                { class: "label", config: editLink.value },
+                { class: "vp-meta-label", config: editLink.value },
                 { before: () => h(EditIcon) },
               ),
             )
           : null,
-        h("div", { class: "meta-item git-info" }, [
+        h("div", { class: "vp-meta-item git-info" }, [
           updateTime.value
             ? h("div", { class: "update-time" }, [
-                h("span", { class: "label" }, `${metaLocales.lastUpdated}: `),
+                h(
+                  "span",
+                  { class: "vp-meta-label" },
+                  `${metaLocales.lastUpdated}: `,
+                ),
                 h(ClientOnly, () =>
-                  h("span", { class: "info" }, <string>updateTime.value),
+                  h(
+                    "span",
+                    { class: "vp-meta-info" },
+                    <string>updateTime.value,
+                  ),
                 ),
               ])
             : null,
           contributors.value && contributors.value.length
             ? h("div", { class: "contributors" }, [
-                h("span", { class: "label" }, `${metaLocales.contributors}: `),
+                h(
+                  "span",
+                  { class: "vp-meta-label" },
+                  `${metaLocales.contributors}: `,
+                ),
                 contributors.value.map(({ email, name }, index) => [
                   h(
                     "span",
-                    { class: "contributor", title: `email: ${email}` },
+                    { class: "vp-meta-info", title: `email: ${email}` },
                     name,
                   ),
                   index !== contributors.value!.length - 1 ? "," : "",
