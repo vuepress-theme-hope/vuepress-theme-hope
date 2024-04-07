9cb475e306d4054cebc69a2754e47a385f4240aa
Mr.Hope <mister-hope@outlook.com>
2024-04-07 16:40:43 +0800

    feat(shared): improve hash function

--- packages/shared/src/client/utils/hash.ts
+++ packages/shared/src/client/utils/hash.ts
@@ -1,18 +1,20 @@
-export const generateIndexFromHash = (
-  content: string,
-  total: number,
-): number => {
-  let hash = 1;
+export const cyrb53 = (content: string, seed = 0): number => {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
 
-  for (let i = 0; i < content.length; i++) {
-    // Jenkins one-at-a-time
-    hash += content.charCodeAt(i);
-    hash += hash << 10;
-    hash ^= hash >> 6;
+  for (let i = 0, ch; i < content.length; i++) {
+    ch = content.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
   }
 
-  hash += hash << 3;
-  hash ^= hash >> 11;
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
 
-  return hash % total;
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
+
+export const generateIndexFromHash = (content: string, total: number): number =>
+  cyrb53(content) % total;
