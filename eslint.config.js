f446b8dd57b7317d4b35abc9ad34515a15b00e9d
Mister-Hope <mister-hope@outlook.com>
2024-04-22 17:12:19 +0800

    chore: tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,6 @@ export default tseslint.config(
       "packages/create/template/**",
       "**/.vuepress/.cache/",
       "**/.vuepress/.temp/",
-      "gulpfile.js",
     ],
   },
   {
@@ -182,7 +181,6 @@ export default tseslint.config(
         },
       ],
       "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
-      // "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/prefer-nullish-coalescing": "warn",
       "vue/custom-event-name-casing": "error",
@@ -259,7 +257,7 @@ export default tseslint.config(
     },
   },
   {
-    files: ["scripts/**.js", "**/gulpfile.js"],
+    files: ["scripts/**.ts", "**/gulpfile.js"],
     languageOptions: {
       globals: globals.node,
     },
