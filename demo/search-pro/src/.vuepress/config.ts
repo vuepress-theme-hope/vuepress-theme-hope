7394dd90b9c6964fa1b834b60cd274bf547066a3
zjkal <javachina@foxmail.com>
2024-04-25 14:01:27 +0800

    Update config.ts
    Simplified searchPro sample code

--- demo/search-pro/src/.vuepress/config.ts
+++ demo/search-pro/src/.vuepress/config.ts
@@ -1,7 +1,5 @@
-import { defaultTheme } from "@vuepress/theme-default";
-import { cut } from "nodejs-jieba";
+import {hopeTheme} from "vuepress-theme-hope";
 import { defineUserConfig } from "vuepress/cli";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 const base = (process.env["BASE"] as "/" | `/${string}/`) || "/";
 
@@ -12,28 +10,12 @@ export default defineUserConfig({
 
   description: "VuePress Client Search plugin",
 
-  theme: defaultTheme({
-    logo: "https://theme-hope-assets.vuejs.press/logo.svg",
-
-    repo: "vuepress-theme-hope/vuepress-theme-hope/tree/main/demo/search-pro/",
-
-    navbar: ["/", "/demo"],
-  }),
-
-  plugins: [
-    searchProPlugin({
-      indexContent: true,
-      hotReload: true,
-      customFields: [
-        {
-          getter: ({ frontmatter }): string[] => frontmatter["tag"] as string[],
-          formatter: `Tag: $content`,
+  theme: hopeTheme({
+        plugins: {
+            searchPro: true,
+            // searchPro: {
+            //   插件选项
+            // },
         },
-      ],
-      indexOptions: {
-        tokenize: (text, fieldName) =>
-          fieldName === "id" ? [text] : cut(text, true),
-      },
     }),
-  ],
 });
