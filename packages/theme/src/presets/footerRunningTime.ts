5fd0e251a3e471669a752c9a12026ec5a094ceab
Mister-Hope <mister-hope@outlook.com>
2024-04-30 16:11:51 +0800

    chore(theme): improve jsdoc

--- packages/theme/src/presets/footerRunningTime.ts
+++ packages/theme/src/presets/footerRunningTime.ts
@@ -4,32 +4,21 @@ import { usePageData, useRouteLocale } from "vuepress/client";
 
 const millisecondPerDay = 1000 * 60 * 60 * 24;
 
+/**
+ *
+ * @param date - The date to calculate the running time
+ * @param locales - The locales of running time, `:day`, `:hour`, `:minute`, `:second` will be replaced by the corresponding value
+ * @param [preserveContent=false] - Whether to preserve the original content of the footer
+ *
+ * @param date - 计算运行时间的日期
+ * @param locales - 运行时间的本地化文字， `:day`, `:hour`, `:minute`, `:second` 会被对应的值替换
+ * @param [preserveContent=false] - 是否保留页脚的原有内容
+ */
 export const setupRunningTimeFooter = (
-  /**
-   * The date to calculate the running time
-   *
-   * 计算运行时间的日期
-   */
   date: string | Date,
-  /**
-   * The locales of running time
-   * @description :day, :hour, :minute, :second will be replaced by the corresponding value
-   *
-   * 运行时间的本地化文字
-   *
-   * @description :day, :hour, :minute, :second 会被对应的值替换
-   */
   locales: Record<string, string> = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     "/": "Running time: :day days :hour hours :minute minutes :second seconds",
   },
-  /**
-   * Whether to preserve the original content of the footer
-   *
-   * 是否保留页脚的原有内容
-   *
-   * @default false
-   */
   preserveContent = false,
 ): void => {
   const initialTimeStamp = (
