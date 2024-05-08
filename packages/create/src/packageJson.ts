6e966c5a0ae00c832b7d52471c66600d721f10ae
Mr.Hope <mister-hope@outlook.com>
2024-05-09 04:09:06 +0800

    chore: bump deps

--- packages/create/src/packageJson.ts
+++ packages/create/src/packageJson.ts
@@ -58,7 +58,7 @@ export const createPackageJson = async ({
   const scripts = getScript(packageManager, bundler, source);
   const devDependencies = {
     [`@vuepress/bundler-${bundler}`]: "2.0.0-rc.9",
-    vue: "^3.4.26",
+    vue: "^3.4.27",
     vuepress: "2.0.0-rc.9",
     "vuepress-theme-hope": version,
   };
