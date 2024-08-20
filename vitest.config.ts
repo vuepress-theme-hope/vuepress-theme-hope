b3e591eee00d1f2dc08929a155c456f28a179a9b
Mr.Hope <mister-hope@outlook.com>
2024-08-20 14:50:42 +0800

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["**/*.spec.ts"],
     coverage: {
       exclude: ["demo/**", "docs/**", "docs-shared/**", "scripts/**"],
+      reporter: ["clover", "json"],
     },
-    include: ["**/*.spec.ts"],
   },
 });
