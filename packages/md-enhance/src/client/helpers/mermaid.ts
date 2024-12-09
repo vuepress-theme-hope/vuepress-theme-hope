48b5df2bc5fb6bdd9b91129943ccb8a574aac0b8
Mister-Hope <mister-hope@outlook.com>
2024-12-09 12:41:51 +0800

    feat(md-enhance): no longer inject config

--- packages/md-enhance/src/client/helpers/mermaid.ts
+++ packages/md-enhance/src/client/helpers/mermaid.ts
@@ -1,20 +1,9 @@
-import type { App } from "vue";
-import { inject } from "vue";
-
 import type { MermaidOptions } from "../typings/index.js";
 
-declare const __VUEPRESS_DEV__: boolean;
-
 let mermaidOptions: MermaidOptions = {};
-
-const mermaidSymbol = Symbol(__VUEPRESS_DEV__ ? "mermaid" : "");
 
 export const defineMermaidConfig = (options: MermaidOptions): void => {
   mermaidOptions = options;
 };
 
-export const useMermaidOptions = (): MermaidOptions => inject(mermaidSymbol)!;
-
-export const injectMermaidConfig = (app: App): void => {
-  app.provide(mermaidSymbol, mermaidOptions);
-};
+export const useMermaidOptions = (): MermaidOptions => mermaidOptions;
