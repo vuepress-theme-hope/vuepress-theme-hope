7d0e3aabe5cb3a704a62fcdb19fcc7d7023ef7e4
Mister-Hope <mister-hope@outlook.com>
2024-12-09 12:43:51 +0800

    feat(lightgallery): no longer provide config

--- packages/lightgallery/src/client/config.ts
+++ packages/lightgallery/src/client/config.ts
@@ -1,11 +1,7 @@
 import { defineClientConfig } from "vuepress/client";
 
 import LightGallery from "./components/LightGallery.js";
-import { injectLightGalleryConfig } from "./helpers/index.js";
 
 export default defineClientConfig({
-  enhance: ({ app }) => {
-    injectLightGalleryConfig(app);
-  },
   rootComponents: [LightGallery],
 });
