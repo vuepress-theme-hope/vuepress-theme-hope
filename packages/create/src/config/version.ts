e98eb875c142c71e803d0fdef602f9f0a87f58c6
Mister-Hope <mister-hope@outlook.com>
2025-02-25 02:49:34 +0800

    fix(create): fix package.json reading

--- packages/create/src/config/version.ts
+++ packages/create/src/config/version.ts
@@ -1,4 +1,19 @@
-import pkg from "../../package.json" with { type: "json" };
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+
+interface PackageJson extends Record<string, unknown> {
+  version: string;
+  devDependencies: Record<string, string>;
+}
+
+const pkg = JSON.parse(
+  readFileSync(
+    fileURLToPath(
+      import.meta.resolve("create-vuepress-theme-hope/package.json"),
+    ),
+    "utf-8",
+  ),
+) as PackageJson;
 
 export const vuepressVersion = pkg.devDependencies.vuepress;
 export const version = pkg.version;
