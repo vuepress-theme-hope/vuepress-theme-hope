cc9f5fa9e6effbd247bbb9cc0fe801cf00149f0b
Mister-Hope <mister-hope@outlook.com>
2025-03-04 13:59:54 +0800

    build: bump deps

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,31 +1,14 @@
-import { execSync } from "node:child_process";
 import { readdirSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
-import type { UserConfig } from "cz-git";
-
 const packages = readdirSync(
   join(dirname(fileURLToPath(import.meta.url)), "./packages/"),
 );
-
-const scopeComplete = execSync("git status --porcelain || true")
-  .toString()
-  .trim()
-  .split("\n")
-  .find((r) => ~r.indexOf("M  packages"))
-  ?.replace(/\//g, "%%")
-  .match(/packages%%((\w|-)*)/)?.[1];
 
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-enum": [2, "always", ["demo", "release", ...packages]],
   },
-  prompt: {
-    defaultScope: scopeComplete,
-    customScopesAlign: !scopeComplete ? "top" : "bottom",
-    allowCustomIssuePrefix: false,
-    allowEmptyIssuePrefix: false,
-  },
-} as UserConfig;
+};
