e04a6a599707df1b8d7a8d493479e81fca0a9db6
Mister-Hope <mister-hope@outlook.com>
2026-03-02 17:03:53 +0800

    chore: tweaks

--- packages/theme/src/client/exports/noop.ts
+++ packages/theme/src/client/exports/noop.ts
@@ -2,7 +2,7 @@ declare const __VP_CUSTOM__: boolean;
 
 if (__VP_CUSTOM__) {
   throw new Error(`\
-VuePress Theme is running in  bundle mode, this import is not allowed.
+VuePress Theme is running in bundle mode, this import is not allowed.
 `);
 }
 
