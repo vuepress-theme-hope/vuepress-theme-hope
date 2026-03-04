018d049469b3883c5b1e528286873ca13b3048bd
Mister-Hope <mister-hope@outlook.com>
2026-03-04 16:21:43 +0800

    chore: tweaks

--- packages/theme/tsdown.config.ts
+++ packages/theme/tsdown.config.ts
@@ -1,12 +1,15 @@
-import { getDirname, path } from "vuepress/utils";
+import { fs, getDirname, path } from "vuepress/utils";
 import { tsdownConfig } from "../../scripts/tsdown.js";
 
 const __dirname = getDirname(import.meta.url);
 
+const presetFiles = (await fs.readdir(path.join(__dirname, "src/presets"))).filter((file) =>
+  file.endsWith(".ts"),
+);
+
 export default tsdownConfig(
   Object.fromEntries([
     ["node/index", "src/node/index.ts"],
-    ["shared/index", "src/shared/index.ts"],
     ...[
       "client/exports/base",
       "client/exports/blog",
@@ -15,12 +18,13 @@ export default tsdownConfig(
       "client/blog",
       "client/index",
     ].map((file) => [`bundle/${file.slice(7)}`, `src/${file}.ts`]),
+    ...presetFiles.map((file) => [`presets/${file.slice(0, -3)}`, `src/presets/${file}`]),
   ]),
   {
     alias: {
       "@theme-hope/": path.resolve(__dirname, "./src/client"),
     },
-    copy: [["client/styles", "bundle"]],
+    copy: [["client/styles"], ["presets/**/*.s?css", "presets"]],
     moduleSideEffects: (id) =>
       id.endsWith(".css") || id.endsWith(".scss") || id.includes("runTimeCheck"),
   },
